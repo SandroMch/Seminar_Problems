170a0ab3665fad43ff7bd9236a9394b8162d579e
S <s>
2023-09-28 18:54:35 +0400

    {"types":"C","runs":3}

--- Problem_1.java
+++ Problem_1.java
@@ -3,9 +3,15 @@ import stanford.karel.*;
 public class Problem_1 extends Karel {
 	
 	public void run() {
+		for(int i = 0; i < 5; i++) {
+			move();
+		}
+		for(int i = 0; i < 10; i++) {
+			pickBeeper();
+		}
 		move();
-		move();
-	
+		for(int i = 0; i < 10; i++) {
+			putBeeper();
+		}
 	}
-
 }
