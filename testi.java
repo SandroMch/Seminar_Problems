248775b2c18de4e5919a3d559932481321182e1f
S <s>
2023-09-29 16:16:37 +0400

    {"types":"C","runs":1}

--- testi.java
+++ testi.java
@@ -1,4 +1,75 @@
+import stanford.karel.*;
 
-public class testi {
-
+public class testi extends SuperKarel {
+	public void run() {
+		firstStep();
+		while(frontIsClear()) {
+			secondStep();
+		
+}	
+}		
+	
+	
+	private void firstStep() {
+		putBeeper();
+		while(frontIsClear()) {
+			move();
+		}
+		putBeeper();
+		turnAround();
+		pickBeeper();
+		if(frontIsClear()) {
+			move();
+		}
+		putBeeper();
+		while(frontIsClear()) {
+			move();
+		}
+		turnAround();
+		pickBeeper();
+		if(frontIsClear()) {
+			move();
+		}
+		putBeeper();
+		if(frontIsClear()) {
+		move();
+		}
+	}
+	private void secondStep() {
+		while(noBeepersPresent()) {
+			if(frontIsClear()) {
+				move();
+			}
+		}
+		turnAround();
+		pickBeeper();
+		if(frontIsClear()) {
+			move();
+		}
+		if(noBeepersPresent()) {
+			putBeeper();
+		}
+		if(frontIsClear()) {
+			move();
+		}
+		while(noBeepersPresent()) {
+			if(frontIsClear()) {
+			move();
+			}
+		}
+		pickBeeper();
+		turnAround();
+		if(frontIsClear()) {
+			move();
+		}
+		if(noBeepersPresent()) {
+			putBeeper();
+		}
+		if(frontIsClear()) {
+		move();
+		}
+	}
+		
 }
+	
+
