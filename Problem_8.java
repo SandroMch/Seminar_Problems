0aa492f92a92168d6e930b032163db4df25b7aa1
S <s>
2023-10-02 16:57:23 +0400

    {"types":"C","runs":2}

--- Problem_8.java
+++ Problem_8.java
@@ -4,8 +4,9 @@ public class Problem_8 extends SuperKarel {
 	public void run() {
 		move();
 		move();
+		substraction();
 	}
-	private void Substraction() {
+	private void substraction() {
 		while(beepersPresent()) {
 			pickBeeper();
 			turnAround();
