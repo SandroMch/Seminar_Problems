2deb83eef7747ec6898361aca1d142ce33980cfc
S <s>
2023-10-05 18:43:38 +0400

    {"types":"C","runs":9}

--- BonusProblem_3.java
+++ BonusProblem_3.java
@@ -1,4 +1,32 @@
+import stanford.karel.SuperKarel;
 
-public class BonusProblem_3 {
-
+public class BonusProblem_3 extends SuperKarel {
+	public void run() {
+		decorateWorld();
+	}
+	
+	private void decorateWorld() {
+		buildOneColumn();
+		while(frontIsClear()) {
+			move();
+			if(frontIsClear()) {
+				move();
+			}
+			buildOneColumn();
+		}
+	}
+	
+	private void buildOneColumn() {
+		turnLeft();
+		putBeeper();
+		for(int i = 0; i < 2; i++) {
+			move();
+			putBeeper();
+		}
+		turnRight();
+		while(frontIsClear()) {
+			move();
+		}
+		turnLeft();
+	}
 }
