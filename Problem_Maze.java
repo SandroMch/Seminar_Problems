058452b405fd1e49029f26f425dcff1cae36eb7e
S <s>
2023-10-05 11:39:16 +0400

    {"types":"C","runs":2}

--- Problem_Maze.java
+++ Problem_Maze.java
@@ -2,6 +2,23 @@ import stanford.karel.*;
 
 public class Problem_Maze extends SuperKarel {
 	public void run() {
-		
+		while(noBeepersPresent()) {
+			findWay();
+		}
+	}
+	private void findWay() {
+		if(frontIsClear()) {
+			move();
+		} else {
+			turnLeft();
+			if(frontIsClear()) {
+				move();
+			} else {
+				turnLeft();
+				if(frontIsClear()) {
+					move();
+				}
+			}
+		}
 	}
 }
