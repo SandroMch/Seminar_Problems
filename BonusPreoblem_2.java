a76cc8a106bca3c4e98de1d6f8e80b299173fb1d
S <s>
2023-10-05 18:34:23 +0400

    {"types":"C","runs":4}

--- BonusPreoblem_2.java
+++ BonusPreoblem_2.java
@@ -1,4 +1,12 @@
+import stanford.karel.SuperKarel;
 
-public class BonusPreoblem_2 {
-
+public class BonusPreoblem_2 extends SuperKarel {
+	public void run() {
+		if(beepersInBag()) {
+			putBeeper();
+		} 
+		if(frontIsClear()) {
+			move();
+		}
+	}
 }
