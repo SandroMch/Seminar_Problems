360aaf37fa4611e6584a698484b80059c9d15939
S <s>
2023-10-05 18:26:14 +0400

    {"types":"C","runs":2}

--- BonusProblem_1.java
+++ BonusProblem_1.java
@@ -2,6 +2,11 @@ import stanford.karel.SuperKarel;
 
 public class BonusProblem_1 extends SuperKarel {
 	public void run() {
-		
+		if(frontIsClear()) {
+			move();
+		} else {
+			turnAround();
+			move();
+		}
 	}
 }
