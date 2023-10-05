6f11e91eb196e0812f3c6d094f5e7ce97cc9d26d
S <s>
2023-10-05 18:55:47 +0400

    {"types":"C","runs":15}

--- BonusProblem_4.java
+++ BonusProblem_4.java
@@ -1,4 +1,21 @@
+import stanford.karel.SuperKarel;
 
-public class BonusProblem_4 {
-
+public class BonusProblem_4 extends SuperKarel {
+	public void run() {
+		for(int i = 0; i < 8; i++ ) {
+			runOneLap();
+		}
+	}
+	private void runOneLap() {
+		for (int i = 0; i < 4; i++ ) {
+			runSide();
+			turnLeft();
+		}
+	}
+	private void runSide() {
+		while(frontIsClear()) {
+			move();
+		}
+		putBeeper();
+	}
 }
