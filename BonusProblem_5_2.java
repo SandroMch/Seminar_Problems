73a53e0c6e8e2a2e2b3b9b985a01f9cc7dd8f318
S <s>
2023-10-06 14:55:22 +0400

    {"types":"C","runs":1}

--- BonusProblem_5_2.java
+++ BonusProblem_5_2.java
@@ -1,4 +1,24 @@
+import stanford.karel.SuperKarel;
 
-public class BonusProblem_5_2 {
-
+public class BonusProblem_5_2 extends SuperKarel {
+	public void run() {
+		arrangeLength();
+		arrangeWidth();
+		takeLengthBack();
+		takeWidthBack();
+		multiplyBeepers();
+		takeToFinalPosition();
+	}
+	private void arrangeLength() {
+		putBeeper();
+		while(frontIsClear()) {
+			move();
+			putBeeper();
+		}
+		turnAround();
+		while(frontIsClear()) {
+			move();
+		}
+		turnAround();
+	}
 }
