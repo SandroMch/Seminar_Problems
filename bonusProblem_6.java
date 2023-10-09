1dd79254c14c5b16c18a40187f7f4b99fdb01cc1
S <s>
2023-10-09 22:37:49 +0400

    {"types":"C","runs":1}

--- bonusProblem_6.java
+++ bonusProblem_6.java
@@ -1,4 +1,35 @@
+import stanford.karel.SuperKarel;
 
-public class bonusProblem_6 {
+public class bonusProblem_6 extends SuperKarel {
+	public void run() {
+		move();
+		move();
+		for(int i = 0; i < 5; i++) {
+			buildHouse();
+			move();
+			move();
+		}
+		drawSun();
+	}
 
+	private void buildHouse() {
+		buildWalls();
+		buildDoor();
+		buildRoof();
+		buildWindows();
+		buildChimney();
+	}
+
+	private void buildWalls() {
+		buildLeftWall();
+		buildRightWall();
+	}
+
+	private void buildLeftWall() {
+		turnLeft();
+		for(int i = 0; i < 9; i++) {
+			putBeeper();
+			move();
+		}
+	}
 }
