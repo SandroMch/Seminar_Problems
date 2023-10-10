46711768c761ba06db80b64ee69872883f8f41bd
S <s>
2023-10-10 19:05:31 +0400

    {"types":"C","runs":36}

--- Problem_12.java
+++ Problem_12.java
@@ -14,5 +14,7 @@ public class Problem_12 extends GraphicsProgram {
 		add(window, 335, 220);
 		window.setFilled(true);
 		window.setColor(Color.CYAN);
+		GRect door = new GRect (30, 100);
+		add(door, 335, 120);
 	}
 }
