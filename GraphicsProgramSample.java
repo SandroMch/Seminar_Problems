49826c1391aa8affe4bb871605fb21bef81f4116
S <s>
2023-10-10 18:39:58 +0400

    {"types":"C","runs":4}

--- GraphicsProgramSample.java
+++ GraphicsProgramSample.java
@@ -4,14 +4,14 @@
  * This program is a stub for the GraphicsHierarchy problem, which
  * draws a partial diagram of the acm.graphics hierarchy.
  */
-
-import acm.graphics.GRect;
+import acm.graphics.*;
 import acm.program.GraphicsProgram;
 
 public class GraphicsProgramSample extends GraphicsProgram {
 	
 	public void run() {
 		GRect myRect = new GRect(100, 200);
-		add(new GRect(100, 200), 69 , 69 );
+		add(new GRect(100, 200), 100 , 50 );
+		
 	}
 }
