023cca3e7d31e0f1546c8573838e08b6dd4b93f0
S <s>
2023-10-10 19:49:18 +0400

    {"types":"C","runs":1}

--- Problem_12.java
+++ Problem_12.java
@@ -5,22 +5,6 @@ import acm.program.GraphicsProgram;
 
 public class Problem_12 extends GraphicsProgram {
 	public void run() {
-		GOval house = new GOval(350,350);
-		add(house , 200,120);
-		house.setFilled(true);
-		house.setColor(Color.yellow);
 		
-		GRect window = new GRect(70, 75);
-		add(window, 335, 220);
-		window.setFilled(true);
-		window.setColor(Color.CYAN);
-		GOval door = new GOval (60, 100);
-		add(door, 340, 370);
-		door.setFilled(true);
-		door.setColor(Color.darkGray);
-		GOval doorHandle = new GOval (15,15);
-		add(doorHandle, 355,400);
-		doorHandle.setFilled(true);
-		doorHandle.setColor(Color.white);
 	}
 }
