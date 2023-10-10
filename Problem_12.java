d0887ed2b1a2ec30fa5b835e5f5564ba993aea10
S <s>
2023-10-10 23:17:23 +0400

    {"types":"C","runs":14}

--- Problem_12.java
+++ Problem_12.java
@@ -7,5 +7,7 @@ public class Problem_12 extends GraphicsProgram {
 	public void run() {
 		GRect house = new GRect(350, 250);
 		add(house, 200,218);
+		GRect windowOne = new GRect(50,50);
+		add(windowOne, 240, 23);
 	}
 }
