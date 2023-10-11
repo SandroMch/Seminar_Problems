8e254b6d49388ff513dac83df08446ec04c5668c
S <s>
2023-10-11 18:41:28 +0400

    {"types":"C","runs":6}

--- problem15.java
+++ problem15.java
@@ -5,7 +5,7 @@ public class problem15 extends ConsoleProgram {
 		int n1 = readInt();
 		int n2 = readInt();
 		int total = n1 + n2;
-		int arithmeticMiddle = total/2;
+		double arithmeticMiddle = total/2;
 		println(arithmeticMiddle);
 	}
 }
