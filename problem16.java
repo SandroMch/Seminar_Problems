dee8f0983b1a04fb3647eaf5435741fd207c384c
S <s>
2023-10-11 19:04:50 +0400

    {"types":"C","runs":17}

--- problem16.java
+++ problem16.java
@@ -6,9 +6,9 @@ public class problem16 extends ConsoleProgram {
 		int B = readInt ("Second Number: ");
 		int C = readInt ("Third Number: ");
 		
-		int upsideA = A;
-		int upsideB = B;
-		int upsideC = C;
+		int upsideA = 1/A;
+		int upsideB = 1/B;
+		int upsideC = 1/C;
 		
 		int middleHarmonic = upsideA + upsideB + upsideC;
 		println(middleHarmonic);
