f7dac43c200f88c861a5017fa557a6ef6262159a
S <s>
2023-10-11 14:12:50 +0400

    {"types":"C","runs":11}

--- Problem_14.java
+++ Problem_14.java
@@ -3,18 +3,16 @@ import acm.program.GraphicsProgram;
 
 public class Problem_14 extends GraphicsProgram {
 	public void run() {
-		findCenter();
-	}
-	private void findCenter() {
-		double width = getWidth();
-		double x = getWidth()/2;
-		
-		double length = getHeight();
-		double y = getHeight()/2;
-		
+		drawFace();
 	}
 	private void drawFace() {
+		double x = getWidth()/2;
+		
+		double y = getHeight()/2;
+		
 		GRect face = new GRect(350,300);
 		add(face , x-175 , y-150);
+		
 	}
+
 }
