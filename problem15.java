876690d1f44c3e45c524ff01de4fe6b30ab6a781
S <s>
2023-10-11 18:42:28 +0400

    {"types":"C","runs":7}

--- problem15.java
+++ problem15.java
@@ -2,8 +2,8 @@ import acm.program.*;
 
 public class problem15 extends ConsoleProgram {
 	public void run() {
-		int n1 = readInt();
-		int n2 = readInt();
+		int n1 = readInt("first Number" );
+		int n2 = readInt("SecondNumber" );
 		int total = n1 + n2;
 		double arithmeticMiddle = total/2;
 		println(arithmeticMiddle);
