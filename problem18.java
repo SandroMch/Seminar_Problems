080511b2a7e38aab7e1dfd6dbdd4ac03f21d6ccb
S <s>
2023-10-11 21:55:11 +0400

    {"types":"C","runs":1}

--- problem18.java
+++ problem18.java
@@ -1,4 +1,13 @@
+import acm.program.*;
 
-public class problem18 {
-
+public class problem18 extends ConsoleProgram {
+	public void run() {
+		int a = readInt("first Number: ");
+		int b = readInt("Second Number: ");
+		if(a > b) {
+			println(a);
+		} else {
+			println(b);
+		}
+	}
 }
