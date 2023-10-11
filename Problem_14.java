2684c358b8c9dd25225d4ffe56500b53eeae31bc
S <s>
2023-10-11 15:07:55 +0400

    {"types":"C","runs":31}

--- Problem_14.java
+++ Problem_14.java
@@ -1,7 +1,7 @@
 import acm.graphics.*;
 import acm.program.GraphicsProgram;
-import javafx.scene.paint.Color;
-
+//import javafx.scene.paint.Color;
+import java.awt.Color;
 public class Problem_14 extends GraphicsProgram {
 	public void run() {
 		drawFace();
@@ -12,22 +12,20 @@ public class Problem_14 extends GraphicsProgram {
 	}
 	private void drawFace() {
 		double x = getWidth()/2;
-		
 		double y = getHeight()/2;
-		
 		GRect face = new GRect(350,300);
 		add(face , x-175 , y-150);
 		face.setFilled(true);
-		face.setFilled(Color.AQUAMARINE);
+		//face.setColor(Color.AQUAMARINE);
 		
 	}
 	private void drawleftEye() {
 		double x = getWidth()/2;
-		
 		double y = getHeight()/2;
-		
 		GOval leftEye = new GOval (50,50);
 		add(leftEye , x-100, y-80);
+		leftEye.setFilled(true);
+		leftEye.setColor(Color.black);
 	}
 	private void drawrightEye() {
 		double x = getWidth()/2;
