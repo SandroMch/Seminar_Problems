764d408c7d954cd4f57a0bdfbfb53c88a757f867
S <s>
2023-10-11 18:56:15 +0400

    {"types":"C","runs":11}

--- problem16.java
+++ problem16.java
@@ -1,4 +1,17 @@
+import acm.program.*;
 
-public class problem16 {
-
+public class problem16 extends ConsoleProgram {
+	public void run() {
+		int A = readInt ("First Number: ");
+		int B = readInt ("Second Number: ");
+		int C = readInt ("Third Number: ");
+		
+		int upsideA = readInt (1/A);
+		int upsideB = readInt (1/B);
+		int upsideC = readInt (1/C);
+		
+		println(upsideA);
+		
+	//	int middleHaromin = readInt()
+	}
 }
