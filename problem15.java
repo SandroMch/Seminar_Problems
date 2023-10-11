4b91ac40d3036a2caabd2d7b76b7565d664fd63f
S <s>
2023-10-11 15:28:09 +0400

    {"types":"C","runs":43}

--- problem15.java
+++ problem15.java
@@ -4,6 +4,7 @@ public class problem15 extends ConsoleProgram{
 	public void run(){
 		int x = readInt("Enter the first number : ");
 		int y = readInt("Enter the second number : ");
-		println((double)x/2+y/2);
+		double n = (x+y)/2;
+		println(n);
 	}
 }
