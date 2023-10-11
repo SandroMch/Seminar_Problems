a6f63bb57c1df08736045c8f8997d2bf1e89d47e
S <s>
2023-10-11 19:03:18 +0400

    {"types":"C","runs":15}

--- problem16.java
+++ problem16.java
@@ -3,15 +3,16 @@ import acm.program.*;
 public class problem16 extends ConsoleProgram {
 	public void run() {
 		int A = readInt ("First Number: ");
-//		int B = readInt ("Second Number: ");
-//		int C = readInt ("Third Number: ");
+		int B = readInt ("Second Number: ");
+		int C = readInt ("Third Number: ");
 		
-		double upsideA = 1/A;
-//		int upsideB = readInt (1/B);
-//		int upsideC = readInt (1/C);
+		int upsideA = 1/A;
+		int upsideB = 1/B;
+		int upsideC = 1/C;
 		
-		println(upsideA);
+		int middleHarmonic = upsideA + upsideB + upsideC;
+		println(middleHarmonic);
 		
-	//	int middleHaromin = readInt()
+	
 	}
 }
