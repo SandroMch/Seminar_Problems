43b3e684e93f1e97af37cce7ca5b39a245b0c67b
S <s>
2023-10-11 13:54:58 +0400

    {"types":"C","runs":1}

--- Problem_14.java
+++ Problem_14.java
@@ -1,4 +1,18 @@
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
 
-public class Problem_14 {
-
+public class Problem_14 extends GraphicsProgram {
+	public void run() {
+		findCenter();
+	}
+	private void findCenter() {
+		double width = getWidth();
+		double x = getWidth()/2;
+		
+		double length = getHeight();
+		double y = getHeight()/2;
+		GLine horizontal = new GLine (0 , y , width , y);
+		add(horizontal);
+		
+	}
 }
