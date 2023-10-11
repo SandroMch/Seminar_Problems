cb5bfaf0dcc8493b3ec784659ea327afa37ec691
S <s>
2023-10-11 15:34:18 +0400

    {"types":"C","runs":44}

--- problem16.java
+++ problem16.java
@@ -1,4 +1,14 @@
-
-public class problem16 {
-
+import acm.program.*;
+public class problem16 extends ConsoleProgram{
+	public void run(){
+		double a = readDouble("Enter the first number : ");
+		double b = readDouble("Enter the second number : ");
+		double c = readDouble("Enter the third number : ");
+		double ans = harmoniuli(a,b,c);
+		println(ans);
+	}
+	double harmoniuli(double a , double b , double c){
+		double t = 1/a + 1/b + 1/c;
+		return 1/t;
+	}
 }
