49bc7d7dd93e10714a41e38005e41e343852644d
S <s>
2023-10-11 18:38:23 +0400

    {"types":"C","runs":2}

--- problem15.java
+++ problem15.java
@@ -1,4 +1,9 @@
+import acm.program.*;
 
-public class problem15 {
-
+public class problem15 extends ConsoleProgram {
+	public void run() {
+		int n1 = readInt();
+		int n2 = readInt();
+		int total = n1 + n2;
+	}
 }
