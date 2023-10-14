6bc37a119d1cf791f899f1f57a5ab168a629c582
S <s>
2023-10-14 20:31:51 +0400

    {"types":"C","runs":3}

--- problem21.java
+++ problem21.java
@@ -3,7 +3,12 @@ import acm.program.*;
 
 public class problem21 extends GraphicsProgram {
 	public void run() {
-		GLine line = new GLine(100,100 , 100, 400);
-		add(line);
+		addLines();
+	}
+	private void addLines() {
+		for(int i = 0; i < 10; i++) {
+			GLine line = new GLine(100,100 , 100, 400);
+			add(line);
+		}
 	}
 }
