eec33f1394c9d7472d3f2bcdd0e54430e5acd249
S <s>
2023-10-14 21:05:36 +0400

    {"types":"C","runs":21}

--- problem21.java
+++ problem21.java
@@ -4,19 +4,14 @@ import acm.program.*;
 public class problem21 extends GraphicsProgram {
 	public static final int i = 20;
 	public static final int n =20;
+	
 	public void run() {
-		while(i < 400) {
-			addLines();
-		}
+		addVertical();
 	}
-	private void addLines() {
-		for(int i = 20; i < 650; i+=50) {
-			GLine vertical = new GLine(i,100 ,i, 400);
+	private void addVertical() {
+		for(int i = 20; i < 530; i+=50) {
+			GLine vertical = new GLine(i,20 ,i, 520);
 			add(vertical);	
-		}
-		for(int n = 20; n < 650; n+=50) {
-			GLine horizontal = new GLine (100 , n , 600 , n);
-			add(horizontal);
 		}
 	}
 }
