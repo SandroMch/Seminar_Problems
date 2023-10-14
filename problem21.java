7dc16ef35b778415d599cfcee7bc279c7aaabfaa
S <s>
2023-10-14 21:07:57 +0400

    {"types":"C","runs":22}

--- problem21.java
+++ problem21.java
@@ -7,11 +7,18 @@ public class problem21 extends GraphicsProgram {
 	
 	public void run() {
 		addVertical();
+		addHorizontal();
 	}
 	private void addVertical() {
 		for(int i = 20; i < 530; i+=50) {
 			GLine vertical = new GLine(i,20 ,i, 520);
 			add(vertical);	
+		}
+	}
+	private void addHorizontal() {
+		for(int n = 20; n < 530; n+=50) {
+			GLine horizontal = new GLine (20, n, 520,n);
+			add(horizontal);
 		}
 	}
 }
