c168962eb830f133ebbf447af16c5ec9683682c6
S <s>
2023-10-14 16:28:04 +0400

    {"types":"C","runs":5}

--- problem17.java
+++ problem17.java
@@ -4,7 +4,7 @@ public class problem17 extends ConsoleProgram {
 	public void run() {
 		double n = readDouble("Enter real number: ");
 		int a = (int) n;
-		println("Whole part of the number"+ a );
+		println("Whole part of the number is: "+ a );
 		
 	}
 }
