3fabebdcb4467fb8489210b2662174a21d8d769f
S <s>
2023-10-14 16:58:53 +0400

    {"types":"C","runs":17}

--- problem20.java
+++ problem20.java
@@ -2,14 +2,10 @@ import acm.program.*;
 
 public class problem20 extends ConsoleProgram {
 	public void run() {
-			int n = readInt("Enter number of addends: ");
-			int sum = 0;
-			for (int i = 0; i < n; i++){
-				int a = readInt("Enter new addend: ");
-				sum += a; // same as sum = sum + a; 
-				// println(sum);
-			}
-			println("Sum: " + sum);
+		int n = readInt("Enter how man: ");
+		for (int i = 0; i < n; i++) {
+			int sum = readInt("Enter number: ");
+			println(sum);
 		}
 	}
-
+}
