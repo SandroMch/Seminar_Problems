c5117a194e418282b06f253435c621fcaf904c3d
S <s>
2023-10-14 16:59:13 +0400

    {"types":"C","runs":18}

--- problem20.java
+++ problem20.java
@@ -2,7 +2,7 @@ import acm.program.*;
 
 public class problem20 extends ConsoleProgram {
 	public void run() {
-		int n = readInt("Enter how man: ");
+		int n = readInt("Enter how many numbers you want to sum: ");
 		for (int i = 0; i < n; i++) {
 			int sum = readInt("Enter number: ");
 			println(sum);
