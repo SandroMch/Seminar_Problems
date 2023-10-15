4e8473df8179aa4ca31864257eb9f45d5a015023
S <s>
2023-10-15 23:28:10 +0400

    {"types":"C","runs":13}

--- problem23.java
+++ problem23.java
@@ -3,11 +3,6 @@ import acm.program.*;
 public class problem23 extends ConsoleProgram {
 	
 	public void run() {
-		int n = readInt("Enter number: ");
-		int sum = 0;
-		for(int i = 0; i < n; i++) {
-			sum = sum + n;
-		}
-		println("Sum is: " + sum);
+		
 	}
 }
