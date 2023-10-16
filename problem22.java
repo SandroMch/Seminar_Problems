a61d5a87d677b6b0853402922ba0744dbe6ae2cb
S <s>
2023-10-16 20:31:10 +0400

    {"types":"C","runs":24}

--- problem22.java
+++ problem22.java
@@ -4,11 +4,12 @@ import acm.program.GraphicsProgram;
 
 public class problem22 extends GraphicsProgram {
 	public static final int row = 7;
+	public static final int column = 7;
 	public void run() {
 		double squareSize = getHeight() / row;
 		double length = getHeight() + 10;
 		for (int i = 0; i < row; i++) {
-			for (int j = 0; j < length; j+=squareSize) {
+			for (int j = 0; j < column; j+=squareSize) {
 				GRect square = new GRect(squareSize , squareSize);
 				add(square , j,0);
 			}
