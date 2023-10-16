6c5a494099dac93708c99e52640e7e80864e2e61
S <s>
2023-10-16 15:24:28 +0400

    {"types":"C","runs":12}

--- problem22.java
+++ problem22.java
@@ -1,4 +1,19 @@
+import acm.graphics.*;
+import acm.program.ConsoleProgram;
+import java.awt.*;
 
-public class problem22 {
 
+public class problem22 extends ConsoleProgram {
+	public static final int row = 8;
+	public static final int column = 8;
+	public void run() {
+		double squareSize = getHeight() / row;
+		for(int i = 0; i < row; i++) {
+			for(int j = 0; j < column; j++) {
+				double x = i * squareSize;
+				double y = j * squareSize;
+				GRect a = new GRect(x,y, squareSize, squareSize);
+			}
+		}
+	}
 }
