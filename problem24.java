4217f5e817bc15e22c0a86ef4b50e7958947617a
S <s>
2023-10-16 21:13:32 +0400

    {"types":"C","runs":33}

--- problem24.java
+++ problem24.java
@@ -3,8 +3,8 @@ import acm.program.*;
 public class problem24 extends ConsoleProgram {
 	public void run() {
 		int n = readInt("Enter number: ");
-		for(int i = 0; i < n; i++) {
-			if((n % i !=0)) {
+		for(int i = 1; i < n; i++) {
+			if((n %i != 0)) {
 				println(i);
 			}
 		}
