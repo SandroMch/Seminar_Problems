c4c26b4e61485a230e5ad1247985f19004f2ba0f
S <s>
2023-10-16 21:00:18 +0400

    {"types":"C","runs":29}

--- problem23.java
+++ problem23.java
@@ -1,4 +1,11 @@
+import acm.program.*;
 
-public class problem23 {
-
+public class problem23 extends ConsoleProgram {
+	public void run() {
+		int n = readInt("Enter number: "); {
+			double a = (n + 1) / 2; 
+			double b = a*n;
+			println("Sum from 1 to n is: " + b);
+		}
+	}
 }
