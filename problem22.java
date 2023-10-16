14a0c8b2b32bda3da7fae3155626be443a733337
S <s>
2023-10-16 20:27:33 +0400

    {"types":"C","runs":21}

--- problem22.java
+++ problem22.java
@@ -6,13 +6,11 @@ public class problem22 extends GraphicsProgram {
 	public static final int row = 8;
 	public void run() {
 		double squareSize = getHeight() / row;
-		double length = squareSize + 400;
-		for(int i = 0; i < row; i++) {
-			for(int j = 0; j < length; j+=squareSize) {
-				GRect rowSquare = new GRect(squareSize, squareSize);
-				add(rowSquare,j,j);
-				GRect columnSquare = new GRect(squareSize,squareSize);
-				add(columnSquare, j,j);
+		double length = getHeight() + 10;
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < length; j+=squareSize) {
+				GRect square = new GRect(squareSize , squareSize);
+				add(square , j,j);
 			}
 		}
 	}
