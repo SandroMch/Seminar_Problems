bca673364eb0b1f6870c082e82c6a35aa9caafec
S <s>
2023-10-17 18:45:17 +0400

    {"types":"C","runs":2}

--- seminar5problem1.java
+++ seminar5problem1.java
@@ -1,4 +1,19 @@
+import acm.graphics.GLabel;
+import acm.program.*;
+import java.awt.Color;
+import java.awt.Font;
 
-public class seminar5problem1 {
-
+public class seminar5problem1 extends GraphicsProgram {
+	public void run() {
+		double x = getWidth();
+		double y = getHeight();
+		GLabel text = new GLabel("width: " + x + "height: " + y);
+		Font london = new Font("London" , Font.PLAIN, 42);
+		text.setFont(london);
+		text.setColor(Color.RED);
+		double x1 = getWidth() / 2;
+		double y1 = getHeight() / 2;
+		add(text, x1 , y1);
+		
+	}
 }
