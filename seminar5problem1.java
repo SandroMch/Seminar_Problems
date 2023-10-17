1ea0232b072f74a55b423fbfa4798d0e39c365ee
S <s>
2023-10-17 18:49:34 +0400

    {"types":"C","runs":6}

--- seminar5problem1.java
+++ seminar5problem1.java
@@ -7,15 +7,15 @@ public class seminar5problem1 extends GraphicsProgram {
 	public void run() {
 		double x = getWidth();
 		double y = getHeight();
-		GLabel text = new GLabel("width: " + x + "height: " + y);
+		GLabel text = new GLabel("width: " + x + ",height: " + y);
 		Font london = new Font("London" , Font.PLAIN, 42);
 		text.setFont(london);
 		text.setColor(Color.RED);
 		double x1 = getWidth() / 2;
 		double y1 = getHeight() / 2;
 		double textWidth = text.getWidth() / 2;
-		double textHeight = text.getHeight() / 2;
-		add(text, x1 - textWidth , y1 - textHeight);
+	//	double textHeight = text.getHeight() / 2;
+		add(text, x1 - textWidth , y1);
 		
 		
 	}
