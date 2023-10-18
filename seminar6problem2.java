2534baf5c5382f38adda7340fb21b2eb5dd58aee
S <s>
2023-10-18 20:08:24 +0400

    {"types":"C","runs":26}

--- seminar6problem2.java
+++ seminar6problem2.java
@@ -9,7 +9,7 @@ public class seminar6problem2 extends ConsoleProgram {
 		double minus = a - b;
 		double multiplication = a * b;
 		double devide = a / b;
-		if(c == "d") {
+		if(c == ("d")) {
 			println(plus);
 		}
 	}
