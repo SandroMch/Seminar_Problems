2d039fa41cada2af6c094e87f1777dd8a6ec0574
S <s>
2023-10-18 19:11:54 +0400

    {"types":"C","runs":6}

--- seminar6problem2.java
+++ seminar6problem2.java
@@ -4,6 +4,13 @@ public class seminar6problem2 extends ConsoleProgram {
 	public void run() {
 		double a = readDouble("Enter first number: ");
 		double b = readDouble("Enter second number: ");
-		String ch = readLine("Enter symbol: ");
+		String c = readLine("Enter symbol: ");
+		double plus = a + b;
+		double minus = a - b;
+		double multiplication = a * b;
+		double devide = a / b;
+		if(c == "+") {
+			println(plus);
+		}
 	}
 }
