22679aa9eb1e89fe667b4082f6e87b248db86a5c
S <s>
2023-10-19 19:11:07 +0400

    {"types":"C","runs":2}

--- problem21.java
+++ problem21.java
@@ -7,13 +7,13 @@ public class problem21 extends GraphicsProgram {
 	public void run() {
 		for(int i = 0; i < NUM_LINES; i ++) {
 	//		addVertical();
-			addHorizontal();
+			addHorizontal(i);
 		}
 		
 	}
 
 	private void addHorizontal(int i) {
-		double height = (double)getHeight()/ (NUM_LINES -1);	
+		double height = (double)getHeight()/ (NUM_LINES - 1);	
 		double y = i * height;
 		double x1 = 0;
 		double x2 = getWidth();
