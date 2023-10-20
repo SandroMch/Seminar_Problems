d922294be3dae6ad6ff2be4ad6ad3b0b1c3c00a0
S <s>
2023-10-20 15:09:51 +0400

    {"types":"C","runs":4}

--- problem25.java
+++ problem25.java
@@ -1,4 +1,24 @@
+import acm.program.*;
 
-public class problem25 {
+public class problem25 extends ConsoleProgram {
+	public void run() {
+		int a = readInt("Enter first number: "); 
+		int b = readInt("Enter second number: ");
+		lowerNum(a , b);
+		int c = a * b;
+		int d = 0;
+		for(int i = 2; i < lowerNum(a,b); i++) {
+			d = d + c/i ; 
+		}
+		if(d % a !=0) {
+			
+		}
+	}
 
+	private int lowerNum(int a , int b) {
+		if(a < b) {
+			return a;
+		}
+		return b;
+	}
 }
