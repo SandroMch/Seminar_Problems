111789b8ae574d5eb918d0f133b97eae6fb02a59
S <s>
2023-10-20 14:13:16 +0400

    {"types":"C","runs":4}

--- problem23.java
+++ problem23.java
@@ -7,6 +7,6 @@ public class problem23 extends ConsoleProgram {
 		for(int i = 0; i <= n; i++) {
 			sum = sum + i;
 		}
-		println("Sum of numbers from 1 to n: " + sum);	
+		println("Sum of numbers from 1 to n is : " + sum);	
 	}
 }
