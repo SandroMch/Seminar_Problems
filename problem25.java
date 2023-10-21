b0cfb15bf9dbdd126bb38e6dcb67455d5c774641
S <s>
2023-10-21 16:08:29 +0400

    {"types":"C","runs":1}

--- problem25.java
+++ problem25.java
@@ -5,14 +5,11 @@ public class problem25 extends ConsoleProgram {
 		int a = readInt("Enter first number: "); 
 		int b = readInt("Enter second number: ");
 		lowerNum(a , b);
-		int c = a * b;
-		int d = 0;
-		for(int i = 2; i < lowerNum(a,b); i++) {
-			d = d + c/i ; 
+		int c = 0;
+		while(c  % b !=0){
+			c = lowerNum(a , b) + b;
 		}
-		if(d % a !=0) {
-			
-		}
+		println(c);
 	}
 
 	private int lowerNum(int a , int b) {
