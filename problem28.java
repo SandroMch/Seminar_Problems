17499c9ed060cf7513809120e62178520c8fa82c
S <s>
2023-10-21 22:54:05 +0400

    {"types":"C","runs":13}

--- problem28.java
+++ problem28.java
@@ -1,4 +1,13 @@
+import acm.program.*;
 
-public class problem28 {
-
+public class problem28 extends ConsoleProgram {
+	private static final int SENTINEL = -1;
+	public void run() {
+		while(true) {
+			int a = readInt("Enter number: ");
+			if( a == SENTINEL) {
+				break;
+			}
+		}
+	}
 }
