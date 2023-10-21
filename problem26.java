055d6e23403788547390cc7ef057a03fc6f58946
S <s>
2023-10-21 22:33:00 +0400

    {"types":"C","runs":2}

--- problem26.java
+++ problem26.java
@@ -4,14 +4,28 @@ public class problem26 extends ConsoleProgram {
 	public void run() {
 		int a = readInt("Enter first number: ");
 		int b = readInt("Enter second number: ");
-		while(true) {
-			
-		}
+		int multiplication = a * b;
+		int USG = multiplication / USJ(a,b);
+		println(USG);
 	}
-	private int findLower(int a , int b) {
+	private int lowerNum(int a , int b) {
 		if(a < b) {
 			return a;
 		}
 		return b;
+	}
+	private int higherNum(int a , int b) {
+		if(a > b) {
+			return a;
+		}
+		return b;
+	}
+	private int USJ(int a , int b) {
+		int c = 0;
+		c = 0 + lowerNum(a , b);
+		while(c % higherNum(a , b) != 0) {
+			c = c + lowerNum(a,b);
+		}
+		return c;
 	}
 }
