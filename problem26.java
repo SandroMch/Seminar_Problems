67e10d3664236c1427b824bbf6a3a3ab138ca8ca
S <s>
2023-10-21 22:15:07 +0400

    {"types":"C","runs":1}

--- problem26.java
+++ problem26.java
@@ -1,4 +1,17 @@
+import acm.program.*;
 
-public class problem26 {
-
+public class problem26 extends ConsoleProgram {
+	public void run() {
+		int a = readInt("Enter first number: ");
+		int b = readInt("Enter second number: ");
+		while(true) {
+			
+		}
+	}
+	private int findLower(int a , int b) {
+		if(a < b) {
+			return a;
+		}
+		return b;
+	}
 }
