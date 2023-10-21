92a7c8c494bac56f6f3b31cfa29a52e8401056ea
S <s>
2023-10-21 23:00:03 +0400

    {"types":"C","runs":15}

--- problem28.java
+++ problem28.java
@@ -4,12 +4,14 @@ public class problem28 extends ConsoleProgram {
 	private static final int SENTINEL = -1;
 	public void run() {
 		int a = 0;
+		int c = 0;
 		while(true) {
 			int b = readInt("Enter number: ");
-			b = a + b;
+			a = a + b;
 			if( b == SENTINEL) {
 				break;
 			}
 		}
+		println(a);
 	}
 }
