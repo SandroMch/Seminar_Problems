65573a691ab269b36d6fb9fd59692667cfd729b0
S <s>
2023-10-25 22:18:43 +0400

    {"types":"C","runs":20}

--- problem31.java
+++ problem31.java
@@ -3,7 +3,7 @@ public class problem31 extends ConsoleProgram {
 	public void run() {
 		int n = readInt("Enter number you want to check:  ");
 		if(n == 2) {
-			println("Number " + n + "is Prime");
+			println("Number " + n + " is Prime");
 		}
 	}
 }
