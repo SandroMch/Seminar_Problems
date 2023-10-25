5d102c4ce90dad18b0b4f27c9efbb01c8b5e9de3
S <s>
2023-10-25 23:33:19 +0400

    {"types":"C","runs":2}

--- problem32.java
+++ problem32.java
@@ -3,8 +3,17 @@ public class problem32 extends ConsoleProgram {
 	public void run() {
 		println(2);
 		println(3);
-		for(int i = 4; i < 1000000; i++) {
-			
+		int n = 1000000;
+	}
+	private int findPrimes(int n) {
+		int c = 0;
+		for(int i = 1; i < n; i++) {
+			if(n % i !=0) {
+				c = c + 1;
+			}
+			if((n - c) == 2) {
+				return i;
+			}
 		}
 	}
 }
