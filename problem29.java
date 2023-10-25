176078528ceae944aba0767d273e7dc926f9110f
S <s>
2023-10-25 21:08:24 +0400

    {"types":"C","runs":1}

--- problem29.java
+++ problem29.java
@@ -2,6 +2,15 @@ import acm.program.*;
 
 public class problem29 extends ConsoleProgram {
 	public void run() {
-		
+		int n = readInt("Enter number: ");
+		int firstNum = 0;
+		int secondNum = 1;
+		int last;
+		if(n == 1) {
+			println(firstNum);
+		} 
+		for(int i = 0; i <= n; i++) {
+			last = firstNum + secondNum;
+		}
 	}
 }
