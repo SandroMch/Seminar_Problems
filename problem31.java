a62c7f455fcfb9af5ca36ca4f4b12147bbb3b89c
S <s>
2023-10-25 23:38:37 +0400

    {"types":"C","runs":6}

--- problem31.java
+++ problem31.java
@@ -3,19 +3,18 @@ public class problem31 extends ConsoleProgram {
 	public void run() {
 		int n = readInt("Enter number you want to check:  ");
 		if(n == 2) {
-			println("Number " + n + " is Prime");
+			println("Number " + n + " is a Prime");
 		}
-		while(n == 1) {
+		if(n == 1) {
 			println(n + " is neither Prime nor Composite");
 		}
-//		if(n == 1) {
-//			println(n + " is neither Prime nor Composite");
-//		}
 		if((n - nonDevisors(n) == 2)) {
 			println(n + " is a prime number");
 
-		} else {
-			println( n +  " is Composite number");
+		}
+		if((n - nonDevisors(n) > 2)) {
+			println(n + " is a composite number");
+
 		}
 	}
 	private int nonDevisors(int n) {
