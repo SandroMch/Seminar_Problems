9740b24cb4835e0d5839f8d3906755826ae6f408
S <s>
2023-10-26 17:38:53 +0400

    {"types":"C","runs":4}

--- seminar8problem2.java
+++ seminar8problem2.java
@@ -1,4 +1,13 @@
+import acm.graphics.GRect;
+import acm.program.*;
 
-public class seminar8problem2 {
-
+public class seminar8problem2 extends GraphicsProgram {
+	public void run() {
+		add(squareSide(), 100, 100);
+	}
+	private GRect squareSide() {
+		double n = readDouble("Enter length of side: ");
+		GRect rect = new GRect(n,n);
+		return rect;
+	}
 }
