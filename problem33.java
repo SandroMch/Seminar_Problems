50857fcd5ecdf13ec74eed2aeae1d8fd6e02496f
S <s>
2023-10-31 17:20:27 +0400

    {"types":"C","runs":1}

--- problem33.java
+++ problem33.java
@@ -1,4 +1,15 @@
+import acm.graphics.GOval;
+import acm.program.*;
+import acm.util.RandomGenerator;
 
-public class problem33 {
-
+public class problem33 extends GraphicsProgram {
+	public void run() {
+		RandomGenerator r = RandomGenerator.getInstance();
+		double rDouble = r.nextDouble(60,120);
+		GOval oval = new GOval(rDouble,rDouble);
+		add(oval,150,150);
+		
+		
+	}
+	
 }
