ccf054b3ddabaa43664e22d26969ff452abb1196
S <s>
2023-10-31 21:43:28 +0400

    {"types":"C","runs":4}

--- problem38.java
+++ problem38.java
@@ -3,13 +3,12 @@ import acm.util.RandomGenerator;
 public class problem38 extends ConsoleProgram {
 	private RandomGenerator rgen = RandomGenerator.getInstance();
 	public void run() {
-		int currMoney = 1000;
-		int betMoney = readInt("How much you want to bet: ");
-		int numChoose = readInt("Choose number from 0 to 36: ");
-		int roulettNum = rgen.nextInt(0 , 36); 
+		int currMoney = 1000; 
+		println(currMoney);
 		while(true) {
+			int roulettNum = rgen.nextInt(0 , 36);
 			int money = readInt("how much you want to bet: ");
-			int num = readInt("choose num: ");
+			int numChoose = readInt("Choose number from 0 to 36: ");
 		}
 	}
 	
