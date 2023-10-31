1c2fc4713ec6392b1bf3454536d2787732cf415e
S <s>
2023-10-31 17:58:18 +0400

    {"types":"C","runs":23}

--- problem35.java
+++ problem35.java
@@ -6,7 +6,7 @@ public class problem35 extends ConsoleProgram {
 		int head = 1;
 		int tails = 2;
 		RandomGenerator rgen = RandomGenerator.getInstance();
-		for(int i = 0; i < 10000; i++) {
+		for(int i = 0; i < 10000000; i++) {
 			double random = rgen.nextInt(head,tails);
 			if(random == head) {
 				c = c + 1;
