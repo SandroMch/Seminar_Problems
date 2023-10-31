22de38eee15ac6a1ee8a01d2b8d70b34598333d8
S <s>
2023-10-31 18:47:49 +0400

    {"types":"C","runs":60}

--- problem36.java
+++ problem36.java
@@ -1,4 +1,7 @@
-
-public class problem36 {
-
+import acm.graphics.*;
+public class problem36 extends ConsoleProgram {
+	private static final int numSimulations = 10_000;
+	public void run() {
+		
+	}
 }
