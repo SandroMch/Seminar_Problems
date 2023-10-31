48a4850b25a1794d6e8891e77a51fbd15d394b16
S <s>
2023-10-31 17:56:55 +0400

    {"types":"C","runs":18}

--- problem35.java
+++ problem35.java
@@ -1,4 +1,17 @@
-
-public class problem35 {
-
+import acm.program.*;
+import acm.util.RandomGenerator;
+public class problem35 extends ConsoleProgram {
+	public void run() {
+		int c = 0;
+		int head = 1;
+		int tails = 2;
+		RandomGenerator rgen = RandomGenerator.getInstance();
+		for(int i = 0; i < 10000; i++) {
+			double random = rgen.nextInt(head,tails);
+			if(random == head) {
+				c = c + 1;
+			}
+		}
+		println("average head: " + c / head);
+	}
 }
