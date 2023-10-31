3495de80941760bde40d5a4c2157958ed33bd2de
S <s>
2023-10-31 21:48:41 +0400

    {"types":"C","runs":6}

--- problem38.java
+++ problem38.java
@@ -4,17 +4,17 @@ public class problem38 extends ConsoleProgram {
 	private RandomGenerator rgen = RandomGenerator.getInstance();
 	public void run() {
 		int currMoney = 1000; 
-		println(currMoney);
+		println("You have" + currMoney + "$");
 		while(true) {
 			int roulettNum = rgen.nextInt(0 , 36);
 			int betMoney = readInt("how much you want to bet: ");
 			int numChoose = readInt("Choose number from 0 to 36: ");
 			if(roulettNum == numChoose) {
 				currMoney += 2 * betMoney;
-				println("You won: " + 2* betMoney + "You have: " + currMoney);
+				println("You won: " + 2* betMoney + " You have: " + currMoney);
 			} else {
 				currMoney -= betMoney;
-				println("You lost: " + betMoney + "You have: " + currMoney);
+				println("You lost: " + betMoney + " You have: " + currMoney);
 			}
 		}
 	}
