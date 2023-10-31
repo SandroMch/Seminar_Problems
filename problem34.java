c512c003daea2e436447323b5cb8fbcb0f4745f4
S <s>
2023-10-31 18:09:43 +0400

    {"types":"C","runs":9}

--- problem34.java
+++ problem34.java
@@ -5,10 +5,10 @@ import acm.util.RandomGenerator;
 public class problem34 extends GraphicsProgram {
 	public void run() {
 		RandomGenerator rgen = RandomGenerator.getInstance();
-		for(int i = 0; i < 100000000; i++) {
+		for(int i = 0; i < 1000; i++) {
 			int x = rgen.nextInt(0,getWidth());
 			int y = rgen.nextInt(0,getHeight());
-			double radius = rgen.nextDouble(60,90);
+			double radius = rgen.nextDouble(60,170);
 			GOval rOval = new GOval(radius,radius);
 			rOval.setFilled(true);
 			rOval.setColor(rgen.nextColor());
