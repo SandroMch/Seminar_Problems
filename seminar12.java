708a1c985bcd101e46dcfb3b9bfb490485495ddf
S <s>
2023-11-02 17:57:22 +0400

    {"types":"C","runs":39}

--- seminar12.java
+++ seminar12.java
@@ -6,9 +6,22 @@ import acm.graphics.GOval;
 import acm.program.*;
 
 public class seminar12 extends GraphicsProgram {
+	private static final int x = 200;
+	private static final int y = 200;
 	public void run() {
-		int x = 200;
-		int y = 200;
+		
+		double centerX = getWidth()/2 - x/2;
+		double centerY = getHeight()/2 - y/2;
+		
+		while(true) {
+			front();
+			pause(1000);
+			back();
+		}
+		
+
+	}
+	private void front() {
 		double centerX = getWidth()/2 - x/2;
 		double centerY = getHeight()/2 - y/2;
 		GOval head = new GOval(x,y);
@@ -18,6 +31,10 @@ public class seminar12 extends GraphicsProgram {
 		GLabel HEAD = new GLabel("HEAD");
 		HEAD.setFont("LONDON-24");
 		add(HEAD,getWidth()/2 - HEAD.getWidth()/2,getHeight()/2 + HEAD.getAscent()/2);
+	}
+	private void back() {
+		double centerX = getWidth()/2 - x/2;
+		double centerY = getHeight()/2 - y/2;
 		GOval tails = new GOval(x,y);
 		tails.setFilled(true);
 		tails.setColor(Color.green);
@@ -25,17 +42,5 @@ public class seminar12 extends GraphicsProgram {
 		TAILS.setFont("LONDON-24");
 		add(tails, centerX,centerY);
 		add(TAILS,getWidth()/2 - TAILS.getWidth()/2,getHeight()/2 + TAILS.getAscent()/2);
-		
-		while(true) {
-			tails.sendToBack();
-			pause(1000);
-			TAILS.sendToBack();
-			pause(1000);
-			head.sendToBack();
-			pause(1000);
-			HEAD.sendToBack();
-			
-		}
 	}
-	
 }
