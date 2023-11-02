50abe4c2f39868c52ff9a3d4570b8f1d1f05e77b
S <s>
2023-11-02 17:11:38 +0400

    {"types":"C","runs":1}

--- seminar12.java
+++ seminar12.java
@@ -1,4 +1,13 @@
+import acm.graphics.GOval;
+import acm.program.*;
 
-public class seminar12 {
-
+public class seminar12 extends GraphicsProgram {
+	public void run() {
+		int x = 200;
+		int y = 200;
+		double centerX = getWidth()/2 + x/2;
+		double centerY = getHeight()/2 + y/2;
+		GOval head = new GOval(x,x);
+		add(head, centerX,centerY);
+	}
 }
