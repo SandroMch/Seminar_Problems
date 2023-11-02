f8372133b242efd2ff2cb1e2ed8c9ba5f7fda9ac
S <s>
2023-11-02 17:12:34 +0400

    {"types":"C","runs":3}

--- seminar12.java
+++ seminar12.java
@@ -5,8 +5,8 @@ public class seminar12 extends GraphicsProgram {
 	public void run() {
 		int x = 200;
 		int y = 200;
-		double centerX = getWidth()/2 + x/2;
-		double centerY = getHeight()/2 + y/2;
+		double centerX = getWidth()/2 - x/2;
+		double centerY = getHeight()/2 - y/2;
 		GOval head = new GOval(x,y);
 		add(head, centerX,centerY);
 	}
