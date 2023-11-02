d03251604035e73957b59f63039c0d9ba0123d0d
S <s>
2023-11-02 16:18:31 +0400

    {"types":"C","runs":2}

--- karel.java
+++ karel.java
@@ -2,6 +2,14 @@ import stanford.karel.SuperKarel;
 
 public class karel extends SuperKarel {
 	public void run() {
-		
+		turnAround();
+		while(beepersPresent()) {
+			pickBeeper();
+			move();
+			putBeeper();
+			turnAround();
+			move();
+			turnAround();		
+		}
 	}
 }
