a438abb82d14cd617ec621bce0663d0e675bc3ee
S <s>
2023-11-03 15:19:31 +0400

    {"types":"C","runs":18}

--- Animations_test.java
+++ Animations_test.java
@@ -21,7 +21,7 @@ public class Animations_test extends GraphicsProgram {
 		add(circle, 0, yConst);
 		
 		GLabel bounceCount = new GLabel("Bounce Count: ");
-		bounceCount.setFont(new Font("Gothic", Font.ITALIC, 40));
+		bounceCount.setFont(new Font("Gothic A1", Font.ITALIC, 40));
 		add(bounceCount, 0, bounceCount.getAscent());
 		
 		while (xStart != xEnd) {
