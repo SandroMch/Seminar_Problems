c34d7f03d481b4ffcf611276e429172a082e7f99
S <s>
2023-11-03 15:01:27 +0400

    {"types":"C","runs":3}

--- Animations_test.java
+++ Animations_test.java
@@ -3,10 +3,12 @@ import acm.program.GraphicsProgram;
 
 public class Animations_test extends GraphicsProgram {
 	private static final int RADIUS = 50;
-	private static final int Y_CONST = getHeight() / 2 - RADIUS; 
+
 	public void run() {
 		int xStart = 0;
 		int xEnd = getWidth() - 2 * RADIUS; 
+		
+		int yConst = getHeight() / 2 - RADIUS; 
 		
 		
 		GOval circle = new GOval(RADIUS * 2, RADIUS * 2);
