e03ba64ecd6c3faf9cec8002830e267a4b180099
S <s>
2023-11-03 15:28:42 +0400

    {"types":"C","runs":25}

--- Animations_test.java
+++ Animations_test.java
@@ -26,7 +26,7 @@ public class Animations_test extends GraphicsProgram {
 		
 		
 		while (xStart != xEnd) {
-			score = new GLabel(Integer.toString(0));
+			GLabel score = new GLabel(Integer.toString(0));
 			
 			circle.setLocation(xStart + 1, yConst);
 			pause(1);
