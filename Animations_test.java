bcaff244cffbc312680f6c391145c3fc837d62d6
S <s>
2023-11-03 14:57:32 +0400

    {"types":"C","runs":3}

--- Animations_test.java
+++ Animations_test.java
@@ -1,8 +1,9 @@
 import acm.program.GraphicsProgram;
 
 public class Animations_test extends GraphicsProgram {
+	private static final int RADIUS = 50;
 	public void run() {
-		
+		GOval oval = new GOval()
 	}
 
 }
