0b0f2e69aa46853d9e801248e15fc4d2042a3cc3
S <s>
2023-11-03 14:58:37 +0400

    {"types":"C","runs":3}

--- Animations_test.java
+++ Animations_test.java
@@ -5,6 +5,7 @@ public class Animations_test extends GraphicsProgram {
 	private static final int RADIUS = 50;
 	public void run() {
 		GOval circle = new GOval(RADIUS * 2, RADIUS * 2);
+		circle.setFilled(true);
 	}
 
 }
