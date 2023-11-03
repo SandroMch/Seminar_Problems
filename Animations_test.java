aca5b954f66bb4c96df58960dfe0c5056c758ede
S <s>
2023-11-03 16:30:19 +0400

    {"types":"C","runs":3}

--- Animations_test.java
+++ Animations_test.java
@@ -11,42 +11,42 @@ public class Animations_test extends GraphicsProgram {
 
 	public void run() {
 		int xStart = 0;
-		int xEnd = getWidth() - 2 * RADIUS; 
-		
-		int yConst = getHeight() / 2 - RADIUS; 
-		
+		int xEnd = getWidth() - 2 * RADIUS;
+
+		int yConst = getHeight() / 2 - RADIUS;
+
 		GOval circle = new GOval(RADIUS * 2, RADIUS * 2);
 		circle.setFilled(true);
 		circle.setColor(rgen.nextColor());
 		add(circle, 0, yConst);
-		
+
 		GLabel bounceCount = new GLabel("Bounce Count - ");
 		bounceCount.setFont(new Font("Arial", Font.BOLD, 40));
 		add(bounceCount, 0, bounceCount.getAscent());
-		
+
 		int bounce = 0;
 		GLabel score = new GLabel(Integer.toString(bounce));
 		score.setFont(new Font("Arial", Font.BOLD, 40));
-		
+
 		while (xStart != xEnd) {
-			
+
 			add(score, bounceCount.getWidth(), bounceCount.getAscent() + 2);
-			
+
 			circle.setLocation(xStart + 1, yConst);
 			pause(1);
 			xStart++;
-			
+
 			if (xStart == xEnd) {
 				remove(score);
 				bounce++;
 				score = new GLabel(Integer.toString(bounce));
 				score.setFont(new Font("Arial", Font.BOLD, 40));
-				add (score, bounceCount.getWidth(), bounceCount.getAscent() + 2);
+				add(score, bounceCount.getWidth(), bounceCount.getAscent() + 2);
 				circle.setColor(rgen.nextColor());
 				xEnd = 0;
 				while (xStart != xEnd) {
 					circle.setLocation(xStart - 1, yConst);
-					pause(0.00001);
+					pause(1);
 					xStart--;
 				}
 				remove(score);
@@ -54,11 +54,10 @@ public class Animations_test extends GraphicsProgram {
 				score = new GLabel(Integer.toString(bounce));
 				score.setFont(new Font("Arial", Font.BOLD, 40));
 				circle.setColor(rgen.nextColor());
-				xEnd = getWidth() - 2 * RADIUS; 
+				xEnd = getWidth() - 2 * RADIUS;
 			}
 		}
-		
-		
+
 	}
 
 }
