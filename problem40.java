11703904151d4d0c26c7301886111131a0ed4115
S <s>
2023-11-06 11:23:00 +0400

    {"types":"C","runs":12}

--- problem40.java
+++ problem40.java
@@ -1,4 +1,13 @@
+import acm.graphics.GRect;
+import acm.program.*;
 
-public class problem40 {
-
+public class problem40 extends GraphicsProgram {
+	public void run() {
+		GRect rect = new GRect(50,50);
+		add(rect,0,getHeight()/2 - 25);
+		while(rect.getX() < getWidth()- 25) {
+			rect.move(1,0);
+			rect.pause(5);
+		}
+	}
 }
