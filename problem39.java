3bc13d7e66ced4c00dc2b0c6c75822b13e335f9f
S <s>
2023-11-06 11:11:56 +0400

    {"types":"C","runs":1}

--- problem39.java
+++ problem39.java
@@ -1,4 +1,9 @@
+import acm.graphics.GRect;
+import acm.program.*;
 
-public class problem39 {
-
+public class problem39 extends GraphicsProgram {
+	public void run() {
+		GRect rect = new GRect(80,80);
+		add(rect,0,getHeight() + 40);
+	}
 }
