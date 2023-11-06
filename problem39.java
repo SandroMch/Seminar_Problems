f47a511050d6a0c5d8ed5634a702306f949e382c
S <s>
2023-11-06 11:12:24 +0400

    {"types":"C","runs":3}

--- problem39.java
+++ problem39.java
@@ -4,6 +4,6 @@ import acm.program.*;
 public class problem39 extends GraphicsProgram {
 	public void run() {
 		GRect rect = new GRect(80,80);
-		add(rect,0,getHeight() - 40);
+		add(rect,0,getHeight()/2 - 40);
 	}
 }
