7ba1c5c777e44fce233a5df0c2fdd8dda6ba9b94
S <s>
2023-11-06 11:14:09 +0400

    {"types":"C","runs":4}

--- problem39.java
+++ problem39.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+
 import acm.graphics.GRect;
 import acm.program.*;
 
@@ -5,5 +7,9 @@ public class problem39 extends GraphicsProgram {
 	public void run() {
 		GRect rect = new GRect(80,80);
 		add(rect,0,getHeight()/2 - 40);
+		rect.setFilled(true);
+		rect.setColor(Color.cyan);
+		rect.move(1,0);
+		rect.pause(10);
 	}
 }
