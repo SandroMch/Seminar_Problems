62470e09847e4f7afaad576b2067042277c1c7c7
S <s>
2023-11-06 11:23:23 +0400

    {"types":"C","runs":14}

--- problem40.java
+++ problem40.java
@@ -5,7 +5,7 @@ public class problem40 extends GraphicsProgram {
 	public void run() {
 		GRect rect = new GRect(50,50);
 		add(rect,0,getHeight()/2 - 25);
-		while(rect.getX() < getWidth()- 25) {
+		while(rect.getX() < getWidth()- 50) {
 			rect.move(1,0);
 			rect.pause(5);
 		}
