fe155676f7ecd8607df6fadff5453485cc47cbde
S <s>
2023-11-06 11:39:27 +0400

    {"types":"C","runs":18}

--- problem40.java
+++ problem40.java
@@ -6,13 +6,15 @@ public class problem40 extends GraphicsProgram {
 		GRect rect = new GRect(50,50);
 		add(rect,0,getHeight()/2 - 25);
 		rect.setFilled(true);
-		while(rect.getX() < getWidth()- 50) {
-			rect.move(1,0);
-			rect.pause(5);
-		} 
-		while(rect.getX() > 0) {
-			rect.move(-1,0);
-			rect.pause(5);
+		while(true) {
+			while(rect.getX() < getWidth()- 50) {
+				rect.move(1,0);
+				rect.pause(5);
+			} 
+			while(rect.getX() > 0) {
+				rect.move(-1,0);
+				rect.pause(5);
+			}
 		}
 	}
 }
