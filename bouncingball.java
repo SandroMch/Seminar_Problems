9a727bcec0cb5143bee605530ce1f0f140aec1f1
S <s>
2023-11-07 17:08:08 +0400

    {"types":"C","runs":23}

--- bouncingball.java
+++ bouncingball.java
@@ -9,11 +9,17 @@ public class bouncingball extends GraphicsProgram {
 
 	public void run() {
 		GOval ball = new GOval(80, 80);
+		GOval secBall = new GOval(40,40);
 		int x = rgen.nextInt(0, getWidth());
 		int y = rgen.nextInt(0, getHeight());
+		int z = rgen.nextInt(0, getWidth());
+		int m = rgen.nextInt(0, getHeight());
 		ball.setFilled(true);
 		ball.setColor(Color.red);
+		secBall.setFilled(true);
+		secBall.setColor(Color.cyan);
 		add(ball, x, y);
+		add(ball, z, m);
 		int xSpeed = 1;
 		int ySpeed = 1;
 		while (true) {
@@ -25,7 +31,15 @@ public class bouncingball extends GraphicsProgram {
 			if(ball.getY() == getHeight() - 80 || ball.getY() == 0) {
 				ySpeed *= -1;
 			}
+			secBall.move(xSpeed, ySpeed);
+			secBall.pause(5);
+			if(secBall.getX() == getWidth() - 40  || secBall.getX() == 0) {
+				xSpeed *= -1;
+			}
+			if(secBall.getY() == getWidth() - 40 || secBall.getY() == 0) {
+				secBall.move(xSpeed, ySpeed);
+				secBall.pause(5);
+			}
 		}
-
 	}
 }
