418d13b7b521f8a263aa26a8334027df282306a0
S <s>
2023-11-07 15:58:38 +0400

    {"types":"C","runs":1}

--- bouncingball.java
+++ bouncingball.java
@@ -1,4 +1,18 @@
+import java.awt.Color;
 
-public class bouncingball {
+import acm.graphics.GOval;
+import acm.program.*;
+import acm.util.RandomGenerator;
 
+public class bouncingball extends GraphicsProgram {
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+
+	public void run() {
+		GOval ball = new GOval(80, 80);
+		int x = rgen.nextInt(0, getWidth());
+		int y = rgen.nextInt(0, getHeight());
+		ball.setFilled(true);
+		ball.setColor(Color.red);
+		add(ball,x,y);
+	}
 }
