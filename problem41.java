f721295aaefbf440c2c7280b7a04195d2053722b
S <s>
2023-11-09 17:11:44 +0400

    {"types":"C","runs":5}

--- problem41.java
+++ problem41.java
@@ -1,9 +1,10 @@
 import acm.graphics.GOval;
+import acm.graphics.GRoundRect;
 import acm.program.*;
 
 public class problem41 extends GraphicsProgram {
 	public void run() {
-		GOval oval = new GOval(60,10);
-		add(oval,100,100);
+		GRoundRect rect = new GRoundRect(50,20);
+		add(rect,100,100);
 	}
 }
