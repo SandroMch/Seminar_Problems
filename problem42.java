f376048f2101a000b37e8462da063bdde6be60f3
S <s>
2023-11-09 17:43:57 +0400

    {"types":"C","runs":13}

--- problem42.java
+++ problem42.java
@@ -1,4 +1,15 @@
+import com.sun.glass.events.MouseEvent;
 
-public class problem42 {
+import acm.graphics.GLine;
+import acm.program.GraphicsProgram;
 
+public class problem42 extends GraphicsProgram {
+	public void run() {
+		addMouseListeners();
+	}
+	public void mouseClicked(MouseEvent e) {
+		int x = e.getX();
+		GLine line = new GLine(e.getX());
+		add(line);
+	}
 }
