0ba0eca27827de30bae1ea55dd027fe00aca156d
S <s>
2023-11-14 13:43:49 +0400

    {"types":"C","runs":2}

--- problem44.java
+++ problem44.java
@@ -1,4 +1,17 @@
+import acm.program.*;
 
-public class problem44 {
+import java.awt.event.MouseEvent;
 
+import acm.graphics.*;
+
+public class problem44 extends GraphicsProgram {
+	private GOval ball;
+	
+	public void run() {
+		addMouseListeners();
+	}
+	public void mouseClicked(MouseEvent e) {
+		ball = new GOval(60,60);
+		add(ball , e.getX(), e.getY());
+	}
 }
