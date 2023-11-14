a33d67dfa90855c53329b40570152c3eb0c56a6b
S <s>
2023-11-14 13:54:59 +0400

    {"types":"C","runs":14}

--- problem44.java
+++ problem44.java
@@ -8,21 +8,23 @@ import acm.graphics.*;
 public class problem44 extends GraphicsProgram {
 	private int xSpeed = 0;
 	private int ySpeed = 1;
+
 	public void run() {
-		GOval ball = new GOval(60,60);
+		GOval ball = new GOval(60, 60);
 		ball.setFilled(true);
 		ball.setColor(Color.cyan);
 		addMouseListeners();
-		
+
 	}
+
 	public void mouseClicked(MouseEvent e) {
-		GOval ball = new GOval(60,60);
+		GOval ball = new GOval(60, 60);
 		ball.setFilled(true);
 		ball.setColor(Color.cyan);
-		add(ball , e.getX() - 30, e.getY() - 30);
-		while(true) {
-			ball.move(xSpeed, ySpeed);
-			ball.pause(5);
-		}
+		add(ball, e.getX() - 30, e.getY() - 30);
+
+		ball.move(xSpeed, ySpeed);
+		ball.pause(5);
+
 	}
 }
