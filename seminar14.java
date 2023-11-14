6fde315ec4980f747106ff94a75d1c82d3454307
S <s>
2023-11-14 17:16:57 +0400

    {"types":"C","runs":8}

--- seminar14.java
+++ seminar14.java
@@ -1,4 +1,22 @@
+import acm.program.*;
 
-public class seminar14 {
+import java.awt.event.MouseEvent;
 
+import acm.graphics.*;
+
+public class seminar14 extends GraphicsProgram {
+	private GRect rect;
+	private GOval oval;
+	public void run() {
+		addMouseListeners();
+		rect = new GRect(80,80);
+		add(rect, 100,100);
+		oval = new GOval(70,70);
+		add(oval , 200,200);
+	}
+	public void mouseDragged(MouseEvent e) {
+		while(e.getPoint() != null) {
+			rect.setLocation(e.getX(), e.getY());
+		}
+	}
 }
