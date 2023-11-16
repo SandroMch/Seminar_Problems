2349ffca29d9880b9ae111127f54019c006038eb
S <s>
2023-11-16 15:31:42 +0400

    {"types":"C","runs":1}

--- problem45.java
+++ problem45.java
@@ -1,9 +1,21 @@
 import acm.program.*;
 
 public class problem45 extends ConsoleProgram {
+	private String text;
+	private String ch;
+	private int c;
 	public void run() {
-		String text = readLine("Enter text: ");
-		Char ch = readLine("Enter character: ");
+		text = readLine("Enter text: ");
+		ch = readLine("Enter character: ");
+		println(compareStrings());
 	}
-	
+	private int compareStrings() {
+		c = 0;
+		for(int i = 0; i < text.length(); i++) {
+			if(ch.equals(text.charAt(i))) {
+				c += 1;
+			}
+		}
+		return c;
+	}
 }
