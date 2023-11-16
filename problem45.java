b8e7f09481a21fa2a2abea75e6e2660277e30546
S <s>
2023-11-16 17:32:52 +0400

    {"types":"C","runs":10}

--- problem45.java
+++ problem45.java
@@ -4,7 +4,7 @@ public class problem45 extends ConsoleProgram {
 	
 	public void run() {
 		String text = readLine("Enter text: ");
-		char symbol = readLine().charAt(0);
+		char symbol = readLine("Enter symbol: ").charAt(0);
 		int c  = 0;
 		for(int i = 0; i < text.length(); i++ ) {
 			if(symbol == text.charAt(i)) {
