94da964af323265ed8d4735168020788e61f5e43
S <s>
2023-11-16 15:33:15 +0400

    {"types":"C","runs":2}

--- problem45.java
+++ problem45.java
@@ -7,15 +7,15 @@ public class problem45 extends ConsoleProgram {
 	public void run() {
 		text = readLine("Enter text: ");
 		ch = readLine("Enter character: ");
-		println(compareStrings());
+		compareStrings();
 	}
-	private int compareStrings() {
+	private void compareStrings() {
 		c = 0;
 		for(int i = 0; i < text.length(); i++) {
 			if(ch.equals(text.charAt(i))) {
 				c += 1;
 			}
 		}
-		return c;
+		println(c);
 	}
 }
