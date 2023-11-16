8988af57595339989658c2a8b6d3c7a2e49d7868
S <s>
2023-11-16 15:33:49 +0400

    {"types":"C","runs":3}

--- problem45.java
+++ problem45.java
@@ -3,14 +3,13 @@ import acm.program.*;
 public class problem45 extends ConsoleProgram {
 	private String text;
 	private String ch;
-	private int c;
+	private int c = 0;
 	public void run() {
 		text = readLine("Enter text: ");
 		ch = readLine("Enter character: ");
 		compareStrings();
 	}
 	private void compareStrings() {
-		c = 0;
 		for(int i = 0; i < text.length(); i++) {
 			if(ch.equals(text.charAt(i))) {
 				c += 1;
