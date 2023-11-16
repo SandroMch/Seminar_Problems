9a5d721e6283216eb40de3c20265c1deeeed307c
S <s>
2023-11-16 17:57:29 +0400

    {"types":"C","runs":10}

--- problem45.java
+++ problem45.java
@@ -1,13 +1,13 @@
 import acm.program.*;
 
 public class problem45 extends ConsoleProgram {
-	
+
 	public void run() {
 		String text = readLine("Enter text: ");
 		char symbol = readLine("Enter symbol: ").charAt(0);
-		int c  = 0;
-		for(int i = 0; i < text.length(); i++ ) {
-			if(symbol == text.charAt(i)) {
+		int c = 0;
+		for (int i = 0; i < text.length(); i++) {
+			if (symbol == text.charAt(i)) {
 				c++;
 			}
 		}
