39a52da919baeb56799ace045b0e18f314018347
S <s>
2023-11-16 14:24:55 +0400

    {"types":"C","runs":1}

--- problem45.java
+++ problem45.java
@@ -1,4 +1,9 @@
+import acm.program.*;
 
-public class problem45 {
-
+public class problem45 extends GraphicsProgram {
+	public void run() {
+		String text = readLine("Enter text: ");
+		println(text);
+	}
+	
 }
