002496201551d06108d4fb42db7751ceff1684d4
S <s>
2023-11-20 15:06:33 +0400

    {"types":"C","runs":0}

--- LeetCode_remove_duplicates.java
+++ LeetCode_remove_duplicates.java
@@ -3,6 +3,11 @@ import acm.program.ConsoleProgram;
 public class LeetCode_remove_duplicates extends ConsoleProgram {
 	public void run () {
 		String input = readLine("Enter Text : ");
+		println(removeDuplicates(input));
+	}
+	
+	private String removeDuplicates(String input) {
 		
 	}
+	
 }	
