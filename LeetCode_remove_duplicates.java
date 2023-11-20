05a15ff1dca51f93388c3cdcb5b3645d0136a22d
S <s>
2023-11-20 15:05:16 +0400

    {"types":"C","runs":0}

--- LeetCode_remove_duplicates.java
+++ LeetCode_remove_duplicates.java
@@ -1,4 +1,7 @@
+import acm.program.ConsoleProgram;
 
-public class LeetCode_remove_duplicates {
-
-}
+public class LeetCode_remove_duplicates extends ConsoleProgram {
+	public void run () {
+		
+	}
+}	
