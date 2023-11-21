ca70d6104546f4e1c14a3d384a337e1d610489ea
S <s>
2023-11-21 17:27:03 +0400

    {"types":"C","runs":5}

--- problem48.java
+++ problem48.java
@@ -5,20 +5,19 @@ public class problem48 extends ConsoleProgram {
 		String text = readLine("Enter text: ");
 		countEverySymbol(text);
 	}
-	
+
 	private void countEverySymbol(String text) {
-		for(int i = 0; i < text.length(); i++) {
+		for (int i = 0; i < text.length(); i++) {
 			char ch = text.charAt(i);
-			int count = counterSymbol(text ,ch);
-			println(ch + count);
+			int count = counterSymbol(text, ch);
+			println(ch + ":" + count);
 		}
 	}
-	
-	
-	private int counterSymbol(String text , char symbol) {
+
+	private int counterSymbol(String text, char symbol) {
 		int symbolCount = 0;
-		for(int i = 0; i < text.length(); i++) {
-			if(text.charAt(i) == symbol) {
+		for (int i = 0; i < text.length(); i++) {
+			if (text.charAt(i) == symbol) {
 				symbolCount++;
 			}
 		}
