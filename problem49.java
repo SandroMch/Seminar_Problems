3a2a0494ff7ce5482c771079d22c66218dcc36ff
S <s>
2023-11-21 17:53:45 +0400

    {"types":"C","runs":8}

--- problem49.java
+++ problem49.java
@@ -4,16 +4,30 @@ public class problem49 extends ConsoleProgram {
 	public void run() {
 		String number = readLine("Enter number: ");
 		int num = StringToInt(number);
-		println(num);
+		if(checkString(number)) {
+			println(num);
+		}
 	}
 
 	private int StringToInt(String number) {
 		int num = 0;
-		for(int i = 0; i < number.length(); i++) {
+		for (int i = 0; i < number.length(); i++) {
 			char currNum = number.charAt(i);
 			int currDigit = currNum - '0';
 			num = num * 10 + currDigit;
 		}
 		return num;
+	}
+	private boolean checkString(String number) {
+		int num = 0;
+		for(int i = 0; i < number.length(); i++) {
+			char currNum = number.charAt(i);
+			int currDigit = currNum - '0';
+			num += currDigit;
+		}
+		if(num >= 0 && num <= 9) {
+			return true;
+		}
+		return false;
 	}
 }
