e61af7f19d7c73a08a052d2667f8f497cdcc6ad8
S <s>
2023-11-21 14:15:48 +0400

    {"types":"C","runs":3}

--- problem47.java
+++ problem47.java
@@ -1,4 +1,16 @@
+import acm.program.*;
 
-public class problem47 {
-
+public class problem47 extends ConsoleProgram {
+	public void run() {
+		String text = readLine("Enter text: ");
+		println(reverse(text));
+	}
+	private String reverse(String text) {
+		text = text.toLowerCase();
+		String reverse = " ";
+		for(int i = 0; i < text.length(); i++) {
+			reverse += text.charAt(i);
+		}
+		return reverse;
+	}
 }
