19b12be346832b3d755d9523ec1cefe0e4f31611
S <s>
2023-11-21 13:57:48 +0400

    {"types":"C","runs":1}

--- problem46.java
+++ problem46.java
@@ -1,11 +1,19 @@
-import org.w3c.dom.Text;
-
-import acm.program.*;
+import acm.program.ConsoleProgram;
 
 public class problem46 extends ConsoleProgram {
-
 	public void run() {
-
+		String text = readLine("Enter text: ");
+		char ch = 0;
+		println(commonChar(text,ch));
 	}
-
+	private int commonChar(String text, char ch) {
+		int count = 0; 
+		for(int i = 0; i < text.length(); i++) {
+			char symbol = text.charAt(i);
+			if (symbol == ch) {
+				count++;
+			}
+		}
+		return count;
+	}
 }
