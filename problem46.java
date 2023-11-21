f59209b69bc74186d1090b1cd1c63c93dd3a2c88
S <s>
2023-11-21 14:08:41 +0400

    {"types":"C","runs":2}

--- problem46.java
+++ problem46.java
@@ -3,17 +3,33 @@ import acm.program.ConsoleProgram;
 public class problem46 extends ConsoleProgram {
 	public void run() {
 		String text = readLine("Enter text: ");
-		char ch = 0;
-		println(commonChar(text,ch));
+		println(commonChar(text));
 	}
-	private int commonChar(String text, char ch) {
-		int count = 0; 
-		for(int i = 0; i < text.length(); i++) {
+
+	private int countChar(String text, char ch) {
+		int count = 0;
+		for (int i = 0; i < text.length(); i++) {
 			char symbol = text.charAt(i);
 			if (symbol == ch) {
 				count++;
 			}
 		}
 		return count;
+	}
+
+	private char commonChar(String text) {
+		char result = ' ';
+		int charCount = 0;
+		for (int i = 0; i < text.length(); i++) {
+			char currChar = text.charAt(i);
+			int numberOfChar = countChar(text, currChar);
+			
+			
+			if (numberOfChar > charCount) {
+				numberOfChar = charCount;
+				result = currChar;
+			}
+		}
+		return result;
 	}
 }
