7fe5c0d2f4e65275f45902edfa332dc70f17a6a0
S <s>
2023-11-21 17:18:17 +0400

    {"types":"C","runs":2}

--- problem48.java
+++ problem48.java
@@ -3,6 +3,13 @@ import acm.program.*;
 public class problem48 extends ConsoleProgram { 
 	public void run() {
 		String text = readLine("Enter text: ");
+		println(findChar(text));
 	}
-	
+	private char findChar(String text) {
+		char symbol = ' ';
+		for(int i = 0; i < text.length(); i++) {
+			symbol += text.charAt(i);
+		}
+		return symbol;
+	}
 }
