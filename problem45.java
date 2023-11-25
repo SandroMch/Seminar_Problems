0669c33e8a5b47c0f6005d7c93679eb76859709e
S <s>
2023-11-25 21:43:59 +0400

    {"types":"C","runs":1}

--- problem45.java
+++ problem45.java
@@ -5,12 +5,13 @@ public class problem45 extends ConsoleProgram {
 	public void run() {
 		String text = readLine("Enter text: ");
 		char symbol = readLine("Enter symbol: ").charAt(0);
-		int c = 0;
-		for (int i = 0; i < text.length(); i++) {
-			if (symbol == text.charAt(i)) {
-				c++;
+		int count = 0;
+		for(int i = 0; i < text.length(); i++) {
+			if(symbol == text.charAt(i)) {
+				count++;
 			}
 		}
-		println(c);
+		println("Symbol quantity is: " + count);
 	}
+
 }
