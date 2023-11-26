a6012aaa4aac663bccca89c91f15b974195e344c
S <s>
2023-11-26 23:33:14 +0400

    {"types":"C","runs":1}

--- problem49.java
+++ problem49.java
@@ -2,34 +2,17 @@ import acm.program.*;
 
 public class problem49 extends ConsoleProgram {
 	public void run() {
-		String number = readLine("Enter number: ");
-		int num = StringToInt(number);
-		if(checkString(number)) {
-			println(num);
-		} else {
-			println("You must put numbers");
-		}
+		String numbers = readLine("Enter numbers: ");
+		stringToNumber(numbers);
 	}
 
-	private int StringToInt(String number) {
-		int num = 0;
-		for (int i = 0; i < number.length(); i++) {
-			char currNum = number.charAt(i);
-			int currDigit = currNum - '0';
-			num = num * 10 + currDigit;
+	private int stringToNumber(String numbers) {
+		int number = 0;
+		char zero = '0';
+		for (int i = 0; i <numbers.length(); i++) {
+			number = numbers.charAt(i) - zero;
 		}
-		return num;
+		return number;
 	}
-	private boolean checkString(String number) {
-		int num = 0;
-		for(int i = 0; i < number.length(); i++) {
-			char currNum = number.charAt(i);
-			int currDigit = currNum - '0';
-			num += currDigit;
-		}
-		if(num >= 0 && num <= 9) {
-			return true;
-		}
-		return false;
-	}
+	
 }
