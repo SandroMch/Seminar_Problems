4930cf65cadf42cf49dc06e5569d1c1952c9ac0f
S <s>
2023-11-28 00:07:20 +0400

    {"types":"C","runs":5}

--- problem51.java
+++ problem51.java
@@ -11,11 +11,9 @@ public class problem51 extends ConsoleProgram {
 		String noDoubles = "Your text has no double symbols";
 		for (int i = 0; i < text.length(); i++) {
 			char currChar = text.charAt(i);
-			while (true) {
-				if (currChar == text.charAt(i)) {
-					newText = text.substring(0, i) + text.substring(i + 1);
-					return newText;
-				}
+			if (currChar == text.charAt(i + 1)) {
+				newText = text.substring(0 , i) + text.substring(i + 1);
+				return newText;
 			}
 		}
 		return noDoubles;
