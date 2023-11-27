e396bbd732c1f0c390dd0aa07bde8c9b8c1f3b08
S <s>
2023-11-27 23:33:30 +0400

    {"types":"C","runs":11}

--- problem51.java
+++ problem51.java
@@ -1,4 +1,20 @@
+import acm.program.*;
 
-public class problem51 {
+public class problem51 extends ConsoleProgram {
+	public void run() {
+		String text = readLine("Enter text: ");
+		removeDoubles(text);
+	}
 
+	private String removeDoubles(String text) {
+		String newText = "";
+		for (int i = 0; i < text.length(); i++) {
+			char currChar = text.charAt(i);
+			if (currChar == text.charAt(i + 1)) {
+				newText = currChar + text.substring(i + 1);
+				return newText;
+			}
+		}
+		return null;
+	}
 }
