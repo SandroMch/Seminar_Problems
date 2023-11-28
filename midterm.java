d6cfc521c498628d3473594de36b2fa9fd60281d
S <s>
2023-11-28 18:00:45 +0400

    {"types":"C","runs":22}

--- midterm.java
+++ midterm.java
@@ -3,6 +3,7 @@ import acm.program.ConsoleProgram;
 public class midterm extends ConsoleProgram {
 	public void run() {
 		String text = readLine("Enter text: ");
+		println(newText(text));
 	}
 	private String lowerCase(String text) {
 		String newText = "";
