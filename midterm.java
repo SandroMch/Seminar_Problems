fc285d8829053979dad157f9cdb8fe8dc85bdadd
S <s>
2023-11-28 18:04:08 +0400

    {"types":"C","runs":24}

--- midterm.java
+++ midterm.java
@@ -3,18 +3,27 @@ import acm.program.ConsoleProgram;
 public class midterm extends ConsoleProgram {
 	public void run() {
 		String text = readLine("Enter text: ");
-		println(lowerCase(text));
+		println(biggerSymbols(text));
 	}
+
 	private String lowerCase(String text) {
 		String newText = "";
-		for(int i = 0; i < text.length(); i++) {
+		for (int i = 0; i < text.length(); i++) {
 			char currChar = text.charAt(i);
-			if(Character.isLowerCase(currChar)) {
-				newText+= currChar;
+			if (Character.isLowerCase(currChar)) {
+				newText += currChar;
+			}
+		}
+		return newText;
+	}
+	private boolean biggerSymbols(String newText) {
+		for(int i = 1; i < newText.length(); i++) {
+			char ch = newText.charAt(i - 1);
+			if(newText.charAt(i) < ch) {
+				return false;
 			}
 		}
 		
-		
-		return newText;
+		return true;
 	}
 }
