8519b5c99d04231766e1dcc7b2d92e1e479ee1b9
S <s>
2023-11-28 17:40:57 +0400

    {"types":"C","runs":12}

--- problem51.java
+++ problem51.java
@@ -3,19 +3,18 @@ import acm.program.*;
 public class problem51 extends ConsoleProgram {
 	public void run() {
 		String text = readLine("Enter text: ");
-		println(removeDoubles(text));
+
 	}
 
 	private String removeDoubles(String text) {
-		String newText = "";
-		String noDoubles = "Your text has no double symbols";
-		for (int i = 1; i < text.length(); i++) {
-			char currChar = text.charAt(i - 1);
-			if (currChar == text.charAt(i)) {
-				newText = text.substring(0 , i - 1) + text.substring(i);
-				return newText;
+
+		String result = "";
+		for (int i = 0; i > text.length(); i++) {
+			char currChar = text.charAt(i);
+			if (text.indexOf(currChar) == i) {
+				result += currChar;
 			}
 		}
-		return noDoubles;
+		return result;
 	}
 }
