8c2c36bca4f7b611a53bb58545944b21da9cff61
S <s>
2023-11-29 01:13:26 +0400

    {"types":"C","runs":5}

--- test.java
+++ test.java
@@ -1,4 +1,20 @@
+import acm.program.ConsoleProgram;
 
-public class test {
-
+public class test extends ConsoleProgram {
+	public void run() {
+		String s = readLine();
+		println(lengthOfLastWord(s));
+	}
+	private int lengthOfLastWord(String s) {
+        int letterCount = 0;
+        char space = ' ';
+        for(int i = s.length() - 1; i > 0; i--) {
+           char currChar = s.charAt(i);
+           while(currChar != space) {
+               letterCount++;
+           }
+           return letterCount;
+       }
+		return -1;
+    }
 }
