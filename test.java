b6fa08c78146ed5f9c8ac3dcc1261faa28a1bddc
S <s>
2023-11-29 00:34:56 +0400

    {"types":"C","runs":1}

--- test.java
+++ test.java
@@ -1,4 +1,23 @@
+import acm.program.ConsoleProgram;
 
-public class test {
-
+public class test extends ConsoleProgram {
+	public void run() {
+		String haystack = readLine();
+		String needle = readLine();
+		println(strStr(haystack , needle));
+		
+	}
+	public int strStr(String haystack, String needle) {
+        if(needle.length() > haystack.length()) return -1;
+        for(int i = 0; i < haystack.length(); i++) {
+            int needleIndex = 0;
+            if(haystack.charAt(i) == needle.charAt(needleIndex)){
+                needleIndex++;
+            }
+             if(needleIndex == (needle.length()-1)) {
+                    return (i - needleIndex);
+             }
+        }
+        return -1;
+    }
 }
