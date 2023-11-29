6b1f67fa3c2a4c2bd773dcf77696a12816d38da4
S <s>
2023-11-29 21:50:43 +0400

    {"types":"C","runs":36}

--- midter7.java
+++ midter7.java
@@ -1,4 +1,17 @@
+import acm.program.*;
+import acm.util.RandomGenerator;
 
-public class midter7 {
-
+public class midter7 extends ConsoleProgram {
+	RandomGenerator rgen = RandomGenerator.getInstance();
+	public void run() {
+		int win = 0;
+		int lose = 1;
+		println(winOrLose(win , lose));
+	}
+	private boolean winOrLose(int win , int lose) {
+		int gerbi = rgen.nextInt(0 , 1);
+		if(gerbi == win) return true;
+		return false;
+		
+	}
 }
