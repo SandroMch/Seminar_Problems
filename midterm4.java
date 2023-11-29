33a0ff634d563a6b968345ff84918ffa7766a1a9
S <s>
2023-11-29 19:16:13 +0400

    {"types":"C","runs":16}

--- midterm4.java
+++ midterm4.java
@@ -2,20 +2,18 @@ import acm.program.*;
 
 public class midterm4 extends ConsoleProgram {
 	public void run() {
-//		String str = readLine("Enter tex: ");
-		int count = 0;
-		for(int i = 10; i > 0; i--) {
-			count++;
-		}
-		println(count);
+		String str = readLine("Enter text: ");
+		println(reverse(str));
+		
 	}
-//	private String reverse(String str) {
-//		String newStr = "";
-//		int lngth = str.length() - 1;
-//		for(int i = lngth; i > 0; i--) {
-//			String reversed = str.charAt(i) + newStr;
-//			return reversed;
-//		}
-//		return newStr;
-//	}
+	@SuppressWarnings("unused")
+	private String reverse(String str) {
+		String newStr = "";
+		int lngth = str.length() - 1;
+		for(int i = lngth; i > 0; i--) {
+			String reversed = str.charAt(i) + newStr;
+			return reversed;
+		}
+		return newStr;
+	}
 }
