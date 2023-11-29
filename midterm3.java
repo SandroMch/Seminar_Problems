399211ed205cf4eebb829a24ce18a1382e97cadf
S <s>
2023-11-29 18:40:23 +0400

    {"types":"C","runs":1}

--- midterm3.java
+++ midterm3.java
@@ -1,4 +1,31 @@
+import java.util.StringTokenizer;
 
-public class midterm3 {
+import acm.program.*;
+
+public class midterm3 extends ConsoleProgram {
+	StringTokenizer tokenizer;
+
+	public void run() {
+		String str = readLine("Enter text: ");
+		tokenizer = new StringTokenizer(str);
+		println(oneWord(str));
+	}
+
+	private String oneWord(String text) {
+		String noWord = "";
+		while (tokenizer.hasMoreTokens()) {
+			String newStr = tokenizer.nextToken();
+			if (newStr.length() == 1) {
+				newStr = newStr.substring(0, 1).toUpperCase();
+				noWord += newStr;
+			} else {
+				newStr = newStr.substring(0, 1).toUpperCase() + newStr.substring(1);
+
+			}
+			return noWord += newStr;
+
+		}
+		return noWord;
+	}
 
 }
