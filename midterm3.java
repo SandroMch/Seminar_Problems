f1868dc357bb73030c1059e0b8574fe2a125781a
S <s>
2023-11-29 18:46:26 +0400

    {"types":"C","runs":5}

--- midterm3.java
+++ midterm3.java
@@ -6,9 +6,11 @@ public class midterm3 extends ConsoleProgram {
 	StringTokenizer tokenizer;
 
 	public void run() {
-		String str = readLine("Enter text: ");
-		tokenizer = new StringTokenizer(str);
-		println(oneWord(str));
+		while (true) {
+
+			String str = readLine("Enter text: ");
+			println(oneWord(str));
+		}
 	}
 
 	private String oneWord(String str) {
@@ -16,14 +18,14 @@ public class midterm3 extends ConsoleProgram {
 		tokenizer = new StringTokenizer(str);
 		while (tokenizer.hasMoreTokens()) {
 			String newStr = tokenizer.nextToken();
-			if(noWord.length() != 0) {
-				noWord+= ' ';
+			if (noWord.length() != 0) {
+				noWord += ' ';
 			}
 			if (newStr.length() == 1) {
 				newStr = newStr.substring(0, 1).toUpperCase();
 				noWord += newStr;
 			} else {
-				newStr = newStr.substring(0, 1).toUpperCase() + newStr.substring(1);
+				newStr = newStr.substring(0, 1).toUpperCase() + newStr.substring(1).toLowerCase();
 
 			}
 			return noWord += newStr;
