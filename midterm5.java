4a7a61f89252fc655233750c695b6f7698a214cf
S <s>
2023-11-29 20:33:47 +0400

    {"types":"C","runs":29}

--- midterm5.java
+++ midterm5.java
@@ -1,4 +1,33 @@
+import acm.program.*;
 
-public class midterm5 {
+public class midterm5 extends ConsoleProgram {
+	public void run() {
+		String str = readLine();
+		String str2 = readLine();
+		println(isAnagram(str,str2));
+	}
+
+	private int countSymbols(String str,  char ch) {
+		int res = 0;
+		for (int i = 0; i < str.length(); i++) {
+			char currChar = str.charAt(i);
+			if (currChar == ch) {
+				res++;
+			}
+		}
+		return res;
+	}
+	private boolean isAnagram(String str , String str2) {
+		if(str.length() != str2.length()) return false;
+		for(int i = 0; i < str.length(); i++) {
+			char currChar = str.charAt(i);
+			int str1 = countSymbols(str , currChar);
+			int stri2 = countSymbols(str2, currChar);
+			if(str1 == stri2) {
+				return true;
+			}
+		}
+		return false;
+	}
 
 }
