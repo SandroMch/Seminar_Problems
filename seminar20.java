9dea301a58f01f04f855e9e61a25b84fac9cbbd9
S <s>
2023-11-30 17:19:34 +0400

    {"types":"C","runs":10}

--- seminar20.java
+++ seminar20.java
@@ -1,10 +1,16 @@
+import java.awt.event.MouseEvent;
+
+import acm.graphics.GObject;
 import acm.graphics.GOval;
+import acm.graphics.GPoint;
 import acm.program.*;
 import acm.util.RandomGenerator;
 
 public class seminar20 extends GraphicsProgram {
 	GOval ball;
 	RandomGenerator rgen = RandomGenerator.getInstance();
+	GObject xTouch;
+	GObject yTouch;
 	public void run() {
 		int radius = rgen.nextInt(60, 150);
 		int diameter = 2 * radius;
@@ -14,5 +20,19 @@ public class seminar20 extends GraphicsProgram {
 		ball.setFilled(true);
 		ball.setColor(rgen.nextColor());
 		add(ball , spawnPointX,spawnPointY);
+		addMouseListeners();
+	}
+	public void mouseClicked(MouseEvent e) {
+		GPoint location =  ball.getLocation();
+		while(location != null) {
+			ballMoveing();
+		}
+	}
+	private void ballMoveing() {
+		double xSpeed = rgen.nextDouble(1,4);
+		double ySpeed = rgen.nextDouble(1,4);
+		double delay = rgen.nextDouble(4, 7);
+		ball.move(xSpeed,ySpeed);
+		ball.pause(delay);
 	}
 }
