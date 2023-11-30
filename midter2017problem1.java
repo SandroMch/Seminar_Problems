a7acbb4ee8bff01d6ba9dedfee1538500b660ec8
S <s>
2023-11-30 21:11:35 +0400

    {"types":"C","runs":8}

--- midter2017problem1.java
+++ midter2017problem1.java
@@ -1,13 +1,16 @@
+import acm.graphics.GLabel;
 import acm.graphics.GObject;
 import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 
 public class midter2017problem1 extends GraphicsProgram {
 	GObject rect;
+	GObject label;
 	int width = 300;
 	int length = 300;
 	public void run() {
 		rect = new GRect(width,length);
 		add(rect,getWidth()/2 - width/2, getHeight()/2 - length/2);
+		label = new GLabel("0");
 	}
 }
