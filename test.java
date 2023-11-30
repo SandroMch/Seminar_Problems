410936f65e4331746510095598429420f7f9e0c9
S <s>
2023-11-30 20:18:15 +0400

    {"types":"C","runs":2}

--- test.java
+++ test.java
@@ -7,15 +7,16 @@ public class test extends ConsoleProgram{
 		int y = 2;
 		GObject o = new GRect(x,y);
 		String s = "shalaxo";
-		println(iseRa(x,y,o,s));
-		println(iseRa(x,y,o,s));
+		println(s.substring('a'));
+//		println(iseRa(x,y,o,s));
+//		println(iseRa(x,y,o,s));
 	}
 	
-	private int iseRa(int x, double y, GObject r, String s){
-		x++;
-		y += 1;
-		r.move(x,y);
-		s = s.substring(s.indexOf('a'));
-		return (int)(x + y + r.getX() + s.length());
-	}
+//	private int iseRa(int x, double y, GObject r, String s){
+//		x++;
+//		y += 1;
+//		r.move(x,y);
+//		s = s.substring(s.indexOf('a'));
+//		return (int)(x + y + r.getX() + s.length());
+//	}
 }
