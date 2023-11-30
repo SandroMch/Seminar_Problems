39416b29850179bfb35292073eab93154ef2087b
S <s>
2023-11-30 21:38:48 +0400

    {"types":"C","runs":27}

--- midterm2017problem2.java
+++ midterm2017problem2.java
@@ -3,17 +3,18 @@ import acm.program.*;
 public class midterm2017problem2 extends ConsoleProgram {
 	public void run() {
 		int x = readInt();
-		for(int i = 0; i < x; i++) {
-			println(i);
+		for(int i = x; i >= 0; i--) {
+			if(x % i != 0 && isPrime(i)) {
+				println(i);
+				break;
+			}
 		}
 	}
-
 	private boolean isPrime(int x) {
 		double root = Math.sqrt(x);
-		if (x == 2)
-			return true;
-		for (int i = 2; i <= root + 1; i++) {
-			if (x % i == 0) {
+		if(x == 2) return true;
+		for(int i = 2; i <= root + 1; i++) {
+			if(x % i == 0) {
 				return false;
 			}
 		}
