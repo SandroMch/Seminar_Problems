9c50e94b5080fc5cbc30d0de9f28a6cae72f2fec
S <s>
2023-11-30 21:30:28 +0400

    {"types":"C","runs":15}

--- midterm2017problem2.java
+++ midterm2017problem2.java
@@ -7,6 +7,7 @@ public class midterm2017problem2 extends ConsoleProgram {
 	}
 	private boolean isPrime(int x) {
 		double root = Math.sqrt(x);
+		if(x == 2) return true;
 		for(int i = 2; i <= root + 1; i++) {
 			if(x % i == 0) {
 				return false;
