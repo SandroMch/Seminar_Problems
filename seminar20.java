36f8c54c98413112f12866f264b1d01d5bf81ec1
S <s>
2023-11-30 17:06:44 +0400

    {"types":"C","runs":1}

--- seminar20.java
+++ seminar20.java
@@ -1,4 +1,16 @@
+import acm.graphics.GOval;
+import acm.program.*;
+import acm.util.RandomGenerator;
 
-public class seminar20 {
-
+public class seminar20 extends GraphicsProgram {
+	GOval ball;
+	RandomGenerator rgen = RandomGenerator.getInstance();
+	public void run() {
+		int radius = rgen.nextInt(60, 300);
+		int diameter = 2 * radius;
+		double spawnPointX = rgen.nextDouble(0, getWidth() - diameter);
+		double spawnPointY = rgen.nextDouble(0 , getHeight() - diameter);
+		ball = new GOval(diameter,diameter);
+		add(ball , spawnPointX,spawnPointY);
+	}
 }
