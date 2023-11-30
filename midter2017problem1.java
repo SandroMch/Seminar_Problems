2797f87fc9199e6b090e85e8413a3a98480a8301
S <s>
2023-11-30 21:10:53 +0400

    {"types":"C","runs":7}

--- midter2017problem1.java
+++ midter2017problem1.java
@@ -4,8 +4,8 @@ import acm.program.GraphicsProgram;
 
 public class midter2017problem1 extends GraphicsProgram {
 	GObject rect;
-	int width = 150;
-	int length = 150;
+	int width = 300;
+	int length = 300;
 	public void run() {
 		rect = new GRect(width,length);
 		add(rect,getWidth()/2 - width/2, getHeight()/2 - length/2);
