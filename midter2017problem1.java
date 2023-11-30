3ce4860c2368d776106c7e44cc803d85105bc984
S <s>
2023-11-30 21:13:40 +0400

    {"types":"C","runs":10}

--- midter2017problem1.java
+++ midter2017problem1.java
@@ -8,10 +8,11 @@ public class midter2017problem1 extends GraphicsProgram {
 	GObject label;
 	int width = 300;
 	int length = 300;
+	int score = 0;
 	public void run() {
 		rect = new GRect(width,length);
 		add(rect,getWidth()/2 - width/2, getHeight()/2 - length/2);
-		label = new GLabel("0");
+		label = new GLabel("score");
 		add(label, getWidth()/2 - label.getWidth()/2,getHeight()/2 - label.getHeight()/2);
 	}
 }
