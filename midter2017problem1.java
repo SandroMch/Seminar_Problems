ef2bc468cf857b0a01a75497bae843453bd3ebba
S <s>
2023-11-30 21:19:50 +0400

    {"types":"C","runs":11}

--- midter2017problem1.java
+++ midter2017problem1.java
@@ -1,3 +1,5 @@
+import java.awt.event.MouseEvent;
+
 import acm.graphics.GLabel;
 import acm.graphics.GObject;
 import acm.graphics.GRect;
@@ -8,11 +10,14 @@ public class midter2017problem1 extends GraphicsProgram {
 	GObject label;
 	int width = 300;
 	int length = 300;
-	int score = 0;
 	public void run() {
 		rect = new GRect(width,length);
 		add(rect,getWidth()/2 - width/2, getHeight()/2 - length/2);
-		label = new GLabel("score");
+		label = new GLabel("0");
 		add(label, getWidth()/2 - label.getWidth()/2,getHeight()/2 - label.getHeight()/2);
+		addMouseListeners();
+	}
+	public void mouseDragged(MouseEvent e) {
+		
 	}
 }
