b875de8020935f9907ccefdc30d28f5b75d2f86a
S <s>
2023-12-02 15:54:50 +0400

    {"types":"C","runs":0}

--- Midterm_practice.java
+++ Midterm_practice.java
@@ -3,5 +3,9 @@ import acm.program.ConsoleProgram;
 public class Midterm_practice extends ConsoleProgram {
 	public void run() {
 		String input = readLine();
+		
+		for (int i = 0; i < input.length(); i++) {
+			char currChar = input.charAt(i);
+		}
 	}
 }
