0a84cf63f0b5ba831db33c0cf44151f235bf0df7
S <s>
2023-12-02 18:11:05 +0400

    {"types":"C","runs":11}

--- midterm2022problem1.java
+++ midterm2022problem1.java
@@ -1,4 +1,5 @@
 import java.awt.event.*;
+import java.util.StringTokenizer;
 
 import acm.graphics.*;
 import acm.program.*;
@@ -8,7 +9,20 @@ public class midterm2022problem1 extends ConsoleProgram {
 	private GPoint lastPoint = new GPoint(0, 0);
 	
 	public void run () {
-		println(stringToInt("14") + 1);
+		String a = "15 + 30 - 12";
+		
+		StringTokenizer tokenizer = new StringTokenizer(a, " ");
+		int ans = stringToInt(tokenizer.nextToken());
+
+		while(tokenizer.hasMoreTokens()) {
+			String str = tokenizer.nextToken();
+			if (str.equals("+")) {
+				ans += stringToInt(tokenizer.nextToken());
+			} else if (str.equals("-")) {
+				ans -= stringToInt(tokenizer.nextToken());
+			}
+		}
+		println(ans);
 	}
 	
 	private int stringToInt(String a) {
