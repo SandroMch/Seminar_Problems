147442eaddb2188114b0ac1942af2b845e78c914
S <s>
2023-12-02 15:56:18 +0400

    {"types":"C","runs":0}

--- Midterm_practice.java
+++ Midterm_practice.java
@@ -7,7 +7,11 @@ public class Midterm_practice extends ConsoleProgram {
 		for (int i = 0; i < input.length(); i++) {
 			char currChar = input.charAt(i);
 			
-			
+			int n = countChar(input, currChar);
 		}
+	}
+	
+	private int countChar(String input, char currChar) {
+		
 	}
 }
