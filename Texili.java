1fa787f06914cc611299125da7c0a72ebd5361b8
S <s>
2023-12-02 16:04:03 +0400

    {"types":"C","runs":6}

--- Texili.java
+++ Texili.java
@@ -1,6 +1,9 @@
+import acm.graphics.GLine;
 import acm.program.GraphicsProgram;
 
 public class Texili extends GraphicsProgram {
+	GPoint start = new GPoint(0,0);
+	GPoint end;
 	GLine line;
 	public void run() {
 		
