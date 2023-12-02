0525dcf9fde7efc278961710374b388a9e67e0a1
S <s>
2023-12-02 17:00:55 +0400

    {"types":"C","runs":2}

--- midterm2022problem1.java
+++ midterm2022problem1.java
@@ -1,25 +1,25 @@
 import java.awt.event.*;
 
-import acm.graphics.GLine;
+import acm.graphics.*;
 import acm.program.*;
 
 public class midterm2022problem1 extends GraphicsProgram {
-	private GLine prevLine = null;
+	private GLine curLine = null;
+	private GPoint lastPoint = new GPoint(0, 0);
 	
 	public void run () {
 		addMouseListeners();
-		prevLine.setStartPoint(0, 0);
-		add(prevLine);
 	}
 	
 	public void mouseClicked(MouseEvent e) {
-		GLine newLine = new GLine(prevLine.getEndPoint().getX(), prevLine.getEndPoint().getY(), e.getX(), e.getY());
-		prevLine = newLine;
-		add(newLine);
+		GPoint newPoint = new GPoint(e.getX(), e.getY());
+		GLine line = new GLine(lastPoint.getX(), lastPoint.getY(), newPoint.getX(), newPoint.getY());
+		curLine = line;
+		add(line);
 	}
 	
 	public void mouseDragged(MouseEvent e) {
-		prevLine.setEndPoint(e.getX(), e.getY());
+		curLine.setEndPoint(e.getX(), e.getY());
 	}
 	
 }
