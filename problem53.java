3d52ff400ed9d8883e5ccf01b101bdc903d6d110
S <s>
2023-12-05 17:35:46 +0400

    {"types":"C","runs":12}

--- problem53.java
+++ problem53.java
@@ -17,6 +17,7 @@ public class problem53 extends ConsoleProgram {
 				}
 				println("Symbols: " +countSybols(line));
 				println("Words: " + countWord(line));
+				println("Sentence: " + countSentence(line));
 			}
 			
 		} catch (FileNotFoundException e) {
@@ -48,5 +49,16 @@ public class problem53 extends ConsoleProgram {
 			}
 		}
 		return count + 1;
+	}
+	private int countSentence(String line) {
+		int count = 0;
+		char dot = '.';
+		for(int i = 0; i < line.length(); i++) {
+			char currChar = line.charAt(i);
+			if(currChar == dot) {
+				count++;
+			}
+		}
+		return count;
 	}
 }
