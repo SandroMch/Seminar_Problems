911282a904873f322a1065c5368c4af371cac90b
S <s>
2023-12-05 22:53:45 +0400

    {"types":"C","runs":17}

--- problem54.java
+++ problem54.java
@@ -7,6 +7,9 @@ public class problem54 extends ConsoleProgram {
 	public void run() {
 		int n = readInt("Enter number: ");
 		int[] nums = new int[n];
+		for(int i = 0; i < n; i++) {
+			nums[i] = readInt("Enter number: ");
+		}
 		Arrays.sort(nums);
 		println(nums[n-1]);
 		println(nums[n-2]);
