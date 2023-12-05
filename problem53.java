52b769375d24f56649796f62e8b9124951bf6642
S <s>
2023-12-05 22:52:27 +0400

    {"types":"C","runs":16}

--- problem53.java
+++ problem53.java
@@ -7,70 +7,72 @@ import acm.program.*;
 
 public class problem53 extends ConsoleProgram {
 	String line;
+
 	public void run() {
 		try {
 			BufferedReader bf = new BufferedReader(new FileReader("randomText.txt"));
-			while(true) {
+			while (true) {
 				String line = bf.readLine();
-				if(line == null) {
+				if (line == null) {
 					break;
 				}
-				println("Symbols: " +countSybols(line));
+				println("Symbols: " + countSybols(line));
 				println("Words: " + countWord(line));
 				println("Sentence: " + countSentence(line));
 				println("Special Symbols: " + countSpecialSymbols(line));
 			}
 			bf.close();
-			
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
+
 	private int countSybols(String line) {
 		int count = 0;
 		char space = ' ';
-		for(int i = 0; i < line.length(); i++) {
+		for (int i = 0; i < line.length(); i++) {
 			char currChar = line.charAt(i);
-			if(currChar == space) {
+			if (currChar == space) {
 				count++;
 			}
 		}
 		return line.length() - count;
 	}
+
 	private int countWord(String line) {
 		int count = 0;
 		char space = ' ';
-		for(int i = 0; i < line.length(); i++) {
+		for (int i = 0; i < line.length(); i++) {
 			char currChar = line.charAt(i);
-			if(currChar == space) {
+			if (currChar == space) {
 				count++;
 			}
 		}
 		return count + 1;
 	}
+
 	private int countSentence(String line) {
 		int count = 0;
 		char dot = '.';
-		for(int i = 0; i < line.length(); i++) {
+		for (int i = 0; i < line.length(); i++) {
 			char currChar = line.charAt(i);
-			if(currChar == dot) {
+			if (currChar == dot) {
 				count++;
 			}
 		}
 		return count;
 	}
+
 	private int countSpecialSymbols(String line) {
 		int count = 0;
 		char questionMark = '?';
 		char exclamationMark = '!';
 		char dot = '.';
-		for(int i = 0; i < line.length(); i++) {
+		for (int i = 0; i < line.length(); i++) {
 			char currChar = line.charAt(i);
-			if(currChar == questionMark || currChar == exclamationMark || currChar == dot) {
+			if (currChar == questionMark || currChar == exclamationMark || currChar == dot) {
 				count++;
 			}
 		}
--- problem54.java
+++ problem54.java
@@ -1,4 +1,15 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
-public class problem54 {
+import acm.program.*;
 
+public class problem54 extends ConsoleProgram {
+	public void run() {
+		int n = readInt("Enter number: ");
+		int[] nums = new int[n];
+		Arrays.sort(nums);
+		println(nums[n-1]);
+		println(nums[n-2]);
+		
+	}
 }
