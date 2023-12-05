99f99d43f9da3e767081896ad8a9abae4e15b846
S <s>
2023-12-05 17:19:43 +0400

    {"types":"C","runs":2}

--- problem53.java
+++ problem53.java
@@ -1,4 +1,29 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
-public class problem53 {
+import acm.program.*;
 
+public class problem53 extends ConsoleProgram {
+	String line;
+	public void run() {
+		try {
+			BufferedReader bf = new BufferedReader(new FileReader("randomText.txt"));
+			while(line != null) {
+				line = bf.readLine();
+				println(line);
+			}
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+//	private int countWord(String line) {
+//		int count = 
+//	}
 }
