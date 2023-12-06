b3a4a7e5a49cf7b5967913fa21a75f38e202f5b4
S <s>
2023-12-06 22:52:31 +0400

    {"types":"C","runs":21}

--- forpractice.java
+++ forpractice.java
@@ -1,4 +1,12 @@
+import acm.program.*;
 
-public class forpractice {
-
+public class forpractice extends ConsoleProgram {
+	public void run() {
+		String str = "Sandro";
+		char letter = 'm';
+		int n = str.indexOf('n');
+		str = str.replace(str.charAt(n), letter);
+		println(str);
+	}
+	
 }
