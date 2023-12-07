1c74969f25c6aa5106cc7b0e2658498ae8e3b145
S <s>
2023-12-07 17:10:29 +0400

    {"types":"C","runs":16}

--- problem56.java
+++ problem56.java
@@ -1,17 +1,16 @@
 
-
 import java.util.Arrays;
 
 import acm.program.*;
 
 public class problem56 extends ConsoleProgram {
 	int n;
+
 	public void run() {
-		n = readInt("Enter array length: ");
+		int n = readInt("Enter array length: ");
 		int[] nums = new int[n];
 		for(int i = 0; i < n; i++) {
-			nums[i] = readInt("Enter numbers: ");
+			nums[i] = readInt("Enter number: ");
 		}
 	}
-	
 }
