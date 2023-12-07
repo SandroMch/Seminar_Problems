1ef52a6212e75644b79b673a381db44a3d2805ed
S <s>
2023-12-07 17:47:47 +0400

    {"types":"C","runs":24}

--- problem56.java
+++ problem56.java
@@ -1,4 +1,5 @@
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import acm.program.*;
@@ -9,18 +10,33 @@ public class problem56 extends ConsoleProgram {
 	public void run() {
 		int n = readInt("Enter array length: ");
 		int[] nums = new int[n];
-		for(int i = 0; i < n; i++) {
-			nums[i] = readInt("Enter number: ");
-		}
-		for(int i = 0; i < n; i++) {
-			for(int j = i+1; j < n; j++) {
+		findMinMax(nums,n);
+		// for(int i = 0; i < n; i++) {
+		// nums[i] = readInt("Enter number: ");
+		// }
+		// for(int i = 0; i < n; i++) {
+		// for(int j = i+1; j < n; j++) {
+		// if(nums[i] > nums[j]) {
+		// int number = nums[i];
+		// nums[i] = nums[j];
+		// nums[j] = number;
+		// }
+		// }
+		// }
+		// println(Arrays.toString(nums));
+	}
+
+	private void findMinMax(int[] nums, int n) {
+		int currNum = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = i + 1; j < n; j++) {
 				if(nums[i] > nums[j]) {
-					int number = nums[i];
-					nums[i] = nums[j];
-					nums[j] = number;
+					currNum += nums[i];
+					nums[i] = currNum;
 				}
 			}
 		}
 		println(Arrays.toString(nums));
 	}
+
 }
