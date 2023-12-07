b927e190fa14842f56c2bd6d3f887facb8a2c2d9
S <s>
2023-12-07 17:58:39 +0400

    {"types":"C","runs":28}

--- problem56.java
+++ problem56.java
@@ -13,25 +13,12 @@ public class problem56 extends ConsoleProgram {
 		for (int i = 0; i < n; i++) {
 			nums[i] = readInt("Enter number: ");
 		}
-		findMinMax(nums, n);
-		// for(int i = 0; i < n; i++) {
-		// for(int j = i+1; j < n; j++) {
-		// if(nums[i] > nums[j]) {
-		// int number = nums[i];
-		// nums[i] = nums[j];
-		// nums[j] = number;
-		// }
-		// }
-		// }
-		// println(Arrays.toString(nums));
-	}
-
-	private void findMinMax(int[] nums, int n) {
 		for (int i = 0; i < n; i++) {
 			for (int j = i + 1; j < n; j++) {
 				if (nums[i] > nums[j]) {
-					int currNum = nums[i];
-					nums[i] = currNum;
+					int number = nums[i];
+					nums[i] = nums[j];
+					nums[j] = number;
 				}
 			}
 		}
