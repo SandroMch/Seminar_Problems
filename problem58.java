b4a3ff5216ce3118a373999945d1e093c288d371
S <s>
2023-12-12 17:52:26 +0400

    {"types":"C","runs":20}

--- problem58.java
+++ problem58.java
@@ -1,4 +1,13 @@
+import acm.program.*;
 
-public class problem58 {
-
+public class problem58 extends ConsoleProgram {
+	public void run() {
+		int n = 5;
+		int[][] matrix = new int[n][n];
+		int [] arr = new int[n * n];
+		for(int i = 0; i < n * n; i++) {
+			arr[i] = i + 1;
+		}
+		println(arr.toString());
+	}
 }
