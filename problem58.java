817c71f8be4ea08e4175a1f2d2835ee2d5a5bfab
S <s>
2023-12-12 18:01:33 +0400

    {"types":"C","runs":24}

--- problem58.java
+++ problem58.java
@@ -4,15 +4,20 @@ import acm.program.*;
 
 public class problem58 extends ConsoleProgram {
 	public void run() {
-		int n = 5;
+		int n = readInt();
 		int[][] matrix = new int[n][n];
+		for(int i = 0; i < n; i ++) {
+			for(int j = 0; j < n; j++) {
+				matrix[i][j] = readInt(); 
+			}
+		}
 		int[] newMatrix = new int[n * n];
 		for(int i = 0; i < matrix.length; i++) {
 			for(int j = 0; j < matrix[0].length; j++) {
 				newMatrix[i] = matrix[i][j];
 			}
 		}
-		println(Arrays.toString(newMatrix));
+//		println(Arrays.toString(newMatrix));
 
 	}
 
