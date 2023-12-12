a896ec03b86de71f2692404b0234fd9024eedc5b
S <s>
2023-12-12 14:15:58 +0400

    {"types":"C","runs":5}

--- problem57.java
+++ problem57.java
@@ -4,19 +4,32 @@ public class problem57 extends ConsoleProgram {
 	public void run() {
 		String str1 = readLine("Enter text: ");
 		String str2 = readLine("Enter text: ");
-		for(int i = 0; i < str1.length(); i++) {
+		for (int i = 0; i < str1.length(); i++) {
 			char currChar = str1.charAt(i);
-			println(currChar + ":" + countSymbols(str1,currChar));
+			if (firstOccurance(str1, i)) {
+				println(currChar + ":" + countSymbols(str1, currChar));
+			}
 		}
 	}
+
 	private int countSymbols(String str, char ch) {
 		int count = 0;
-		for(int i = 0; i < str.length(); i++) {
+		for (int i = 0; i < str.length(); i++) {
 			char currChar = str.charAt(i);
-			if(ch == currChar ) {
+			if (ch == currChar) {
 				count++;
 			}
 		}
 		return count;
+	}
+
+	private boolean firstOccurance(String str, int index) {
+		for (int i = 0; i < index; i++) {
+			char currChar = str.charAt(i);
+			if (currChar == str.charAt(index)) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
