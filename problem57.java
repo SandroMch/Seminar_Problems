b20bde824105deb0dbcb061407ad1ab1205bac3b
S <s>
2023-12-12 17:22:40 +0400

    {"types":"C","runs":7}

--- problem57.java
+++ problem57.java
@@ -8,54 +8,25 @@ public class problem57 extends ConsoleProgram {
 	public void run() {
 		String str1 = readLine("Enter text: ");
 		String str2 = readLine("Enter text: ");
-		if(countFirstString(str1).equals(countSecondString(str2))) {
-			println(true);
-		}else {
-			println(false);
-		}
+		println(isAnagram(str1,str2));
 	}
 
-	private int countSymbols(String str, char ch) {
-		int count = 0;
+	private boolean isAnagram(String str1, String str2) {
+		int[] countStr1 = countSymbols(str1);
+		int[] countStr2 = countSymbols(str2);
+		if(Arrays.equals(countStr1, countStr2)) {
+			return true;
+		}
+		return false;
+	}
+
+	private int[] countSymbols(String str) {
+		int[] countStr = new int[26];
 		for (int i = 0; i < str.length(); i++) {
-			char currChar = str.charAt(i);
-			if (ch == currChar) {
-				count++;
-			}
+			char ch = str.charAt(i);
+			int index = ch - 'a';
+			countStr[index]++;
 		}
-		return count;
-	}
-
-	private boolean firstOccurance(String str, int index) {
-		for (int i = 0; i < index; i++) {
-			char currChar = str.charAt(i);
-			if (currChar == str.charAt(index)) {
-				return false;
-			}
-		}
-		return true;
-	}
-
-	private ArrayList<Integer> countFirstString(String str1) {
-		ArrayList<Integer> firstStr = new ArrayList<Integer>();
-		for (int i = 0; i < str1.length(); i++) {
-			char currChar = str1.charAt(i);
-			if (firstOccurance(str1,i)) {
-				firstStr.add(countSymbols(str1,currChar));
-			}
-		}
-		firstStr.sort(Comparator.naturalOrder());
-		return firstStr;
-	}
-	private ArrayList<Integer> countSecondString(String str2) {
-		ArrayList<Integer> secondStr = new ArrayList<Integer>();
-		for (int i = 0; i < str2.length(); i++) {
-			char currChar = str2.charAt(i);
-			if (firstOccurance(str2,i)) {
-				secondStr.add(countSymbols(str2,currChar));
-			}
-		}
-		secondStr.sort(Comparator.naturalOrder());
-		return secondStr;
+		return countStr;
 	}
 }
