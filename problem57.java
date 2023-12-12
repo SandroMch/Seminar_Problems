197b694674d898fe907db0261769cf6ea33f4eda
S <s>
2023-12-12 14:07:07 +0400

    {"types":"C","runs":1}

--- problem57.java
+++ problem57.java
@@ -1,4 +1,22 @@
+import acm.program.*;
 
-public class problem57 {
-
+public class problem57 extends ConsoleProgram {
+	public void run() {
+		String str1 = readLine("Enter text: ");
+		String str2 = readLine("Enter text: ");
+		for(int i = 0; i < str1.length(); i++) {
+			char currChar = str1.charAt(i);
+			println(countSymbols(str1,currChar));
+		}
+	}
+	private int countSymbols(String str, char ch) {
+		int count = 0;
+		for(int i = 0; i < str.length(); i++) {
+			char currChar = str.charAt(i);
+			if(ch == currChar ) {
+				count++;
+			}
+		}
+		return count;
+	}
 }
