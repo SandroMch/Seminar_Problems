4765fe15ce45e924820ff083bf02a2f3e72c2f3d
S <s>
2023-12-12 17:54:02 +0400

    {"types":"C","runs":22}

--- problem58.java
+++ problem58.java
@@ -6,10 +6,14 @@ public class problem58 extends ConsoleProgram {
 	public void run() {
 		int n = 5;
 		int[][] matrix = new int[n][n];
+		
+		println(Arrays.toString(arr(n)));
+	}
+	private int[] arr(int n) {
 		int [] arr = new int[n * n];
 		for(int i = 0; i < n * n; i++) {
 			arr[i] = i + 1;
 		}
-		println(Arrays.toString(arr));
+		return arr;
 	}
 }
