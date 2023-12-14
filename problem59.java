48b795558071450a7c7c44ea4b3ab36617dc49e5
S <s>
2023-12-14 18:19:11 +0400

    {"types":"C","runs":1}

--- problem59.java
+++ problem59.java
@@ -1,4 +1,10 @@
+import acm.program.*;
 
-public class problem59 {
-
+public class problem59 extends ConsoleProgram {
+	public void run() {
+		int n = readInt("Column");
+		int m = readInt("Row");
+		int[][] matrix = new int[n][m];
+		
+	}
 }
