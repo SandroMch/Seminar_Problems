e12113c60c4880f0146170f3f42e315e20585bed
S <s>
2023-12-14 19:01:11 +0400

    {"types":"C","runs":10}

--- problem59.java
+++ problem59.java
@@ -4,20 +4,6 @@ public class problem59 extends ConsoleProgram {
 	String text;
 
 	public void run() {
-		int n = readInt("Column: ");
-		int m = readInt("Row: ");
-		int[][] matrix = new int[n][m];
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
-				matrix[i][j] = readInt();
-			}
-		}
-		text = "";
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
-				text+= matrix[i][j];
-			}
-		}
-		print(text);
+		
 	}
 }
