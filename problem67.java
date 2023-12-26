b0878fd2a6b459622cbf254d56a0e992e0cd7039
S <s>
2023-12-26 18:40:52 +0400

    {"types":"C","runs":7}

--- problem67.java
+++ problem67.java
@@ -5,7 +5,7 @@ import acm.program.*;
 public class problem67 extends GraphicsProgram {
 	private JTextField textField;
 
-	public void innit() {
+	public void init() {
 		JTextField address = new JTextField(30);
 		add(address, SOUTH);
 		addActionListeners();
