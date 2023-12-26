3b31753ccd3b8d225d4fb129946ecaacd85aea98
S <s>
2023-12-26 18:51:15 +0400

    {"types":"C","runs":3}

--- problem67.java
+++ problem67.java
@@ -18,12 +18,18 @@ public class problem67 extends GraphicsProgram {
 	public void run() {
 
 	}
+
 	public void actionPerformed(ActionEvent e) {
-		if(e.getSource() == address) {
-			GImage img = new GImage(address.getText());
-			double x = getWidth() / 2 - img.getWidth() / 2;
-			double y = getHeight() / 2 - img.getHeight() / 2;
-			add(img,x,y);
+		if (e.getSource() == address) {
+			try {
+				GImage img = new GImage(address.getText());
+				double x = getWidth() / 2 - img.getWidth() / 2;
+				double y = getHeight() / 2 - img.getHeight() / 2;
+				add(img, x, y);
+				removeAll();
+			} catch (Exception IO) {
+				
+			}
 		}
 	}
 }
