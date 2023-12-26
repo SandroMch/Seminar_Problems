632ca9fb3f9386778369e28eb010a8affe9985c5
S <s>
2023-12-26 19:01:20 +0400

    {"types":"C","runs":10}

--- problem68.java
+++ problem68.java
@@ -1,4 +1,19 @@
+import javax.swing.JButton;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
-public class problem68 {
+import acm.program.*;
 
+public class problem68 extends ConsoleProgram {
+	private JTextArea address;
+	private JButton enter;
+	public void init() {
+		address = new JTextArea();
+		add(address,SOUTH);
+		println(address);
+	}
+	
+	public void run() {
+		
+	}
 }
