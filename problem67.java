b948678420ff2f8047e0236ffbaba8c87fae6cf4
S <s>
2023-12-26 18:45:37 +0400

    {"types":"C","runs":8}

--- problem67.java
+++ problem67.java
@@ -1,17 +1,29 @@
+import java.awt.event.ActionEvent;
+
 import javax.swing.JTextField;
 
+import acm.graphics.GImage;
 import acm.program.*;
 
 public class problem67 extends GraphicsProgram {
-	private JTextField textField;
+	private JTextField address;
 
 	public void init() {
-		JTextField address = new JTextField(30);
+		address = new JTextField(30);
 		add(address, SOUTH);
 		addActionListeners();
+		address.addActionListener(this);
 	}
 
 	public void run() {
 
+	}
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == address) {
+			GImage img = new GImage(address.getText());
+			double x = getWidth() / 2 - img.getWidth() / 2;
+			double y = getHeight() / 2 - img.getHeight() / 2;
+			add(img,x,y);
+		}
 	}
 }
