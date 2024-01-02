51012d79fd0377f8d2e2cf8873960eded8f1d508
S <s>
2024-01-03 02:36:55 +0400

    {"types":"C","runs":17}

--- problem48.java
+++ problem48.java
@@ -1,4 +1,22 @@
+import acm.program.*;
 
-public class problem48 {
+public class problem48 extends ConsoleProgram {
+	String text;
 
+	public void run() {
+		text = readLine("Enter text");
+		println(countSymbols(text));
+	}
+
+	private int countSymbols(String text) {
+		int count = 0;
+		for (int i = 0; i < text.length(); i++) {
+			for (int j = 1; j < text.length(); j++) {
+				if (text.charAt(i) == text.charAt(j)) {
+					count++;
+				}
+			}
+		}
+		return count;
+	}
 }
