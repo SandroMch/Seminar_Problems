8784716f0c2a0e1b134806c680fae61ce9f5ed38
S <s>
2024-01-03 02:10:52 +0400

    {"types":"C","runs":2}

--- problem39.java
+++ problem39.java
@@ -1,17 +1,22 @@
 import java.awt.Color;
 
+import acm.graphics.GOval;
 import acm.graphics.GRect;
 import acm.program.*;
 
 public class problem39 extends GraphicsProgram {
+	GOval oval;
 	public void run() {
-		GRect rect = new GRect(80,80);
-		add(rect,0,getHeight()/2 - 40);
-		rect.setFilled(true);
-		rect.setColor(Color.cyan);
-		while(rect.getX() < getWidth()/2 - 40) {
-			rect.move(1, 0);
-			rect.pause(10);
+		createBall();
+		while(oval.getX() < getWidth()/2 - 40) {
+			oval.move(1, 0);
+			oval.pause(10);
 		}
+	}
+	private void createBall() {
+		oval = new GOval(80,80);
+		add(oval,0,getHeight()/2 - 40);
+		oval.setFilled(true);
+		oval.setColor(Color.cyan);
 	}
 }
