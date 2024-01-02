d6c79e91013e9cae4ff2eda2bc1d4ef3011c8ea0
S <s>
2024-01-03 02:12:09 +0400

    {"types":"C","runs":3}

--- problem40.java
+++ problem40.java
@@ -1,19 +1,20 @@
+import acm.graphics.GOval;
 import acm.graphics.GRect;
 import acm.program.*;
 
 public class problem40 extends GraphicsProgram {
 	public void run() {
-		GRect rect = new GRect(50,50);
-		add(rect,0,getHeight()/2 - 25);
-		rect.setFilled(true);
+		GOval oval = new GOval(50,50);
+		add(oval,0,getHeight()/2 - 25);
+		oval.setFilled(true);
 		while(true) {
-			while(rect.getX() < getWidth()- 50) {
-				rect.move(1,0);
-				rect.pause(2);
+			while(oval.getX() < getWidth()- 50) {
+				oval.move(1,0);
+				oval.pause(2);
 			} 
-			while(rect.getX() > 0) {
-				rect.move(-1,0);
-				rect.pause(2);
+			while(oval.getX() > 0) {
+				oval.move(-1,0);
+				oval.pause(2);
 				
 			}
 			
