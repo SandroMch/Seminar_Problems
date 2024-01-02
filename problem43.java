97617c326bc9885dd9a0432bb7b534d42b1f91ed
S <s>
2024-01-03 02:26:51 +0400

    {"types":"C","runs":11}

--- problem43.java
+++ problem43.java
@@ -12,21 +12,23 @@ public class problem43 extends GraphicsProgram {
 	private static final double xSize = 80;
 	private static final double ySize = 80;
 	private GOval oval;
+
 	public void run() {
 		moveOval();
-		addMouseListeners();	
+		addMouseListeners();
 	}
+
 	public void mouseDragged(MouseEvent e) {
-		GObject ball = getElementAt(e.getX(),e.getY());
-		if(ball != null) {
-			ball.setLocation(e.getX() - xSize/2,e.getY() - ySize/2);
+		GObject ball = getElementAt(e.getX(), e.getY());
+		if (ball != null) {
+			ball.setLocation(e.getX() - xSize / 2, e.getY() - ySize / 2);
 		}
 	}
+
 	private void moveOval() {
-		oval = new GOval(xSize,ySize);
+		oval = new GOval(xSize, ySize);
 		oval.setFilled(true);
 		oval.setColor(Color.cyan);
-		add(oval,rgen.nextDouble(0,getWidth()- 80),rgen.nextDouble(0,getHeight()- 80));
+		add(oval, rgen.nextDouble(0, getWidth() - 80), rgen.nextDouble(0, getHeight() - 80));
 	}
 }
-
