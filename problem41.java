e4f97ce69cee69964d4d092a59c40d0009848fba
S <s>
2024-01-03 02:20:54 +0400

    {"types":"C","runs":6}

--- problem41.java
+++ problem41.java
@@ -8,11 +8,20 @@ import acm.program.*;
 import javafx.scene.input.MouseDragEvent;
 
 public class problem41 extends GraphicsProgram {
+	GOval point;
 	public void run() {
 		addMouseListeners();
 	}
+	public void mousePressed(MouseEvent e) {
+		paintPoint(e.getX(), e.getY());
+	}
 	public void mouseDragged(MouseEvent e) {
-		GOval point = new GOval(1,1);
-		add(point,e.getX(),e.getY());
+		paintPoint(e.getX(),e.getY());
+	}
+	private void paintPoint(int x , int y) {
+		point = new GOval(2,2);
+		point.setFilled(true);
+		add(point , x - 1 , y -1);
+		
 	}
 }
