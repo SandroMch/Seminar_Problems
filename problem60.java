4b3cd9356df9c28fd42da44cfbe387ffd9d0d663
S <s>
2024-01-04 02:09:22 +0400

    {"types":"C","runs":9}

--- problem60.java
+++ problem60.java
@@ -10,14 +10,14 @@ public class problem60 extends ConsoleProgram {
 		while (true) {
 			n = readInt("Enter number: ");
 
-			if(n == -1) {
+			if (n == -1) {
 				break;
 			}
 			nums.add(n);
 		}
 		int m = nums.size();
 		ArrayList<Integer> newNums = new ArrayList<Integer>();
-		for(int i = m - 1; i >= 0; i--) {
+		for (int i = m - 1; i >= 0; i--) {
 			newNums.add(nums.get(i));
 		}
 		println(newNums);
